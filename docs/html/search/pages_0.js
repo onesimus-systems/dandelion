2a9085940505d7cc3cda1bbdb9cec98b483846bd
Lee <keitellf@gmail.com>
2014-02-11 21:59:39 -0600

    Updated docs with new class

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['dandelion_20weblog',['Dandelion Weblog',['../index.html',1,'']]],
-  ['dandelion_203_2e5',['Dandelion 3.5',['../md__c_1__users__lee__documents__git_hub_dandelion__r_e_a_d_m_e.html',1,'']]]
+  ['dandelion',['Dandelion',['../md__w_1_dandy__r_e_a_d_m_e.html',1,'']]]
 ];
