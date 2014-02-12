6d63684145e24a4c5437133181ba34de44b6e6b4
Lee <keitellf@gmail.com>
2014-02-11 21:59:05 -0600

    First step towards category management

--- scripts/categories.php
+++ scripts/categories.php
@@ -9,169 +9,137 @@
 include_once 'dbconnect.php';
 
 // Authenticate user, if fail go to login page
-if (checkLogIn()) {
-	if ($_SESSION['userInfo']['role'] === "admin") {
-		$admin_link = '| <a href="admin.phtml">Administration</a>';
-	}
-	else {
-		$admin_link = '';
-	}
-	
-	if ($_SESSION['userInfo']['role'] !== "guest") {
-		$settings_link = '| <a href="settings.phtml">Settings</a>';
-	}
-	else {
-		$settings_link = '';
-	}
-}
-else {
+if (!checkLogIn()) {
 	header( 'Location: index.php' );
 }
 
-// Connect to DB
-$conn = new dbManage();
-
-$getCategories = 'SELECT * FROM `category`';
-$cat = $conn->queryDB($getCategories, NULL);
-
-/*
-echo '<pre>';
-print_r($getCategories->fetchall(PDO::FETCH_ASSOC));
-echo '</pre>';*/
-
-$depth = isset($_POST['maxdepth']) ? $_POST['maxdepth'] : 0;
-//$parent = isset($_POST['parent']) ? $_POST['parent'] : '';
-
-// Find the deepest node
-// May user this at a later date but for
-// now it was mainly as an exercise
-
-foreach ($cat as $meow) {
-    if (substr_count($meow['ptree'], ":")+1 > $depth) {
-        $depth = substr_count($meow['ptree'], ":")+1;
-    }
+/**
+ * @brief Categories is responsible for displaying and managing categories
+ *
+ * This class displays all categories and manages the addition, deletion,
+ * and manipulation of the same.
+ *
+ * @author Lee Keitel
+ * @date February 11, 2014
+ ***/
+class Categories
+{
+	private $cat = '';
+	private $depth = 0;
+	
+	function __construct() {
+		$conn = new dbManage();
+		$this->depth = isset($_POST['maxdepth']) ? $_POST['maxdepth'] : 0;
+		$getCategories = 'SELECT * FROM `category`';
+		$this->cat = $cat = $conn->queryDB($getCategories, NULL);
+	}
+	
+	public function showAllCats() {
+		$this->largestBranch();
+		$this->makeArrays();
+		$this->popArrays();
+		$this->genSelect();
+		$this->popSelect();
+		
+	}
+	
+	private function largestBranch() {
+		// Find the deepest node
+		foreach ($this->cat as $meow) {
+		    if (substr_count($meow['ptree'], ":")+1 > $this->depth) {
+		        $this->depth = substr_count($meow['ptree'], ":")+1;
+		    }
+		}
+	}
+	
+	private function makeArrays() {
+		// Create $depth+1 many level arrays
+		for ($i = 0; $i < $this->depth+1; $i++) {
+		    $this->{'cid'.($i)} = array();
+		    $this->{'desc'.($i)} = array();
+		    $this->{'ptree'.($i)} = array();
+		}
+	}
+	
+	private function popArrays() {
+		// Separate arrays by depth
+		foreach ($this->cat as $meow) {
+		    for ($i = 0; $i < $this->depth+1; $i++) {
+		        if ($meow['ptree'] == 0) {
+		            array_push($this->desc0, $meow['desc']);
+		            array_push($this->cid0, $meow['cid']);
+		            array_push($this->ptree0, $meow['ptree']);
+		            break;
+		        }
+		    }
+		    for ($i = 0; $i < $this->depth; $i++) {
+		        if (substr_count($meow['ptree'], ":") == $i && $meow['ptree'] != 0) {
+		            array_push($this->{'desc'.($i+1)}, $meow['desc']);
+		            array_push($this->{'cid'.($i+1)}, $meow['cid']);
+		            array_push($this->{'ptree'.($i+1)}, $meow['ptree']);
+		            break;
+		        }
+		    }
+		}
+	}
+	
+	private function genSelect() {
+		// Create the select boxes
+		// Populate the first box, assign the right JS function name
+		for ($i = 1; $i < $this->depth+2; $i++) {
+		    if ($i == 1) {
+		        echo '<select name="cat_'.$i.'" id="cat_'.$i.'" onchange="pop_cat_'.($i+1).'(this)">';
+		        echo '<option value="select">Select:</option>';
+		            foreach ($this->desc0 as $working) {
+		                echo '<option value="'.$working.'">'.$working.'</option>';
+		            }
+		        echo '</select>';
+		    }
+		    elseif ($i < $this->depth+1) {
+		        echo '<select name="cat_'.$i.'" id="cat_'.$i.'" onchange="pop_cat_'.($i+1).'(this)">';
+		        echo '</select>';
+		    }
+		    else {
+		        echo '<select name="cat_'.$i.'" id="cat_'.$i.'">';
+		        echo '</select>';
+		    }
+		}
+	}
+	
+	private function popSelect() {
+		// Populate the select boxes with categories
+		echo '<script type="text/javascript">';
+		for ($i = 0; $i < $this->depth; $i++) { // For each depth
+			// Generate a JS function
+		    echo 'function pop_cat_'.($i+2).'(o) {';
+		    echo 'd=document.getElementById(\'cat_'.($i+2).'\');';
+		    echo 'if(!d){return;}var mitems=new Array();';
+		    
+		    for ($h = 0; $h < count($this->{'desc'.($i)}); $h++) { // For each category in level i
+		        $item = $this->{'desc'.($i)}[$h];
+		        $cid  = $this->{'cid'.($i)}[$h];
+		
+		        echo 'mitems[\''.$item.'\']=[\'Select:\'';
+		        
+		        for ($j = 0; $j < count($this->{'desc'.($i+1)}); $j++) { // For each category in level i+1
+		            $item = $this->{'desc'.($i+1)}[$j];
+		            $pid = $this->{'ptree'.($i+1)}[$j];
+		            $pid = array_reverse(explode(":", $pid)); // Separate the parent tree
+		            if ($pid[0] == $cid) { // If the parent ID matches the current running level i category, add it to the list
+		                echo ',\''.$item.'\'';
+		            }
+		        }
+		        
+		        echo '];';
+		    }
+		    
+		    echo 'd.options.length=0;cur=mitems[o.options[o.selectedIndex].value];if(!cur){return;}d.options.length=cur.length;';
+		    echo 'for(var i=0;i<cur.length;i++){d.options[i].text=cur[i];d.options[i].value=cur[i];}';
+		    echo '}';
+		}
+		echo '</script>';
+	}
 }
 
-// Create $depth+1 many level arrays
-for ($i = 0; $i < $depth+1; $i++) {
-    ${'cid'.($i)} = array();
-    ${'desc'.($i)} = array();
-    ${'ptree'.($i)} = array();
-}
-
-// Separate arrays by depth
-foreach ($cat as $meow) {
-    for ($i = 0; $i < $depth+1; $i++) {
-        if ($meow['ptree'] == 0) {
-            array_push($desc0, $meow['desc']);
-            array_push($cid0, $meow['cid']);
-            array_push($ptree0, $meow['ptree']);
-            break;
-        }
-    }
-    for ($i = 0; $i < $depth; $i++) {
-        if (substr_count($meow['ptree'], ":") == $i && $meow['ptree'] != 0) {
-            array_push(${'desc'.($i+1)}, $meow['desc']);
-            array_push(${'cid'.($i+1)}, $meow['cid']);
-            array_push(${'ptree'.($i+1)}, $meow['ptree']);
-            break;
-        }
-    }
-}
-
-for ($i = 1; $i < $depth+2; $i++) {
-    if ($i == 1) {
-        echo '<select name="cat_'.$i.'" id="cat_'.$i.'" onchange="pop_cat_'.($i+1).'(this)">';
-        echo '<option value="select">Select:</option>';
-            foreach ($desc0 as $working) {
-                echo '<option value="'.$working.'">'.$working.'</option>';
-            }
-        echo '</select>';
-    }
-    elseif ($i < $depth+1) {
-        echo '<select name="cat_'.$i.'" id="cat_'.$i.'" onchange="pop_cat_'.($i+1).'(this)">';
-        echo '</select>';
-    }
-    else {
-        echo '<select name="cat_'.$i.'" id="cat_'.$i.'">';
-        echo '</select>';
-    }
-}
-
-echo '<script type="text/javascript">';
-for ($i = 0; $i < $depth; $i++) {
-    echo 'function pop_cat_'.($i+2).'(o) {';
-    echo 'd=document.getElementById(\'cat_'.($i+2).'\');';
-    echo 'if(!d){return;}var mitems=new Array();';
-    
-    for ($h = 0; $h < count(${'desc'.($i)}); $h++) {
-        $item = ${'desc'.($i)}[$h];
-        $cid  = ${'cid'.($i)}[$h];
-        
-        echo 'mitems[\''.$item.'\']=[\'Select:\'';
-        
-        for ($j = 0; $j < count(${'desc'.($i+1)}); $j++) {
-            $item = ${'desc'.($i+1)}[$j];
-            $pid = ${'ptree'.($i+1)}[$j];
-            $pid = array_reverse(explode(":", $pid));
-            if ($pid[0] == $cid) {
-                echo ',\''.$item.'\'';
-            }
-        }
-        
-        echo '];';
-    }
-    
-    echo 'd.options.length=0;cur=mitems[o.options[o.selectedIndex].value];if(!cur){return;}d.options.length=cur.length;';
-    echo 'for(var i=0;i<cur.length;i++){d.options[i].text=cur[i];d.options[i].value=cur[i];}';
-    echo '}';
-}
-echo '</script>';
-/*
-?>
-<script type="text/javascript">
-    //if(!d){return;}			
-    //var mitems=new Array();
-    mitems['Desktop']=[''];
-    mitems['Appliances']=['Select:','Struxureware','bomgar','campusmgr','copysense','dr4000','fish1','fish2','hr_ibm6400','kace','procerastat','wlc','wlse'];
-    mitems['Network']=['Select:','MonitorLogs','Nexus1','Nexus2','admin','housing','ilight','labs','power','storage','unix'];
-    mitems['Servers']=['Select:','novell','unix','VMservers','windows'];
-    mitems['UPS']=[''];
-    //echo 'd.options.length=0;cur=mitems[o.options[o.selectedIndex].value];if(!cur){return;}d.options.length=cur.length;';
-    //echo 'for(var i=0;i<cur.length;i++){d.options[i].text=cur[i];d.options[i].value=cur[i];}';
-</script>
-<?php*/
-
-/*
-// Populate select boxes with categories
-echo '<form>';
-
-for ($i = 0; $i < $depth+1; $i++) {
-    $turkey =  isset(${'desc'.($i)}[0]) ? ${'desc'.($i)}[0] : ''; // Don't ask
-    $ham =     isset(${'cid'.($i)}[0]) ? ${'cid'.($i)}[0] : ''; // Don't ask
-    $chicken = isset(${'ptree'.($i)}[0]) ? ${'ptree'.($i)}[0] : ''; // Don't ask
-    
-    $tree = explode(":", $chicken);
-    $tree = array_reverse($tree);
-    
-    if ($depth == 0) {
-        echo '<select onChange="showCats('.($i+1). ', this.value)">';
-        foreach ($desc0 as $cat) {
-            echo '<option value='.${'cid'.($i)}[$i].'>'.$cat.'</option>';
-        }
-        echo '</select>';
-    }
-    
-    if ($turkey != '' && $turkey != NULL && $depth != 0) {
-        echo '<select onChange="showCats('.($i+1). ', this.value)">';
-        foreach (${'desc'.($i)} as $cat) {
-            if ($cat == $parent) {
-            echo '<option value='.${'cid'.($i)}[$i].'>'.$cat.'</option>'; }
-        }
-        echo '</select>';
-    }
-}
-echo $parent;
-echo '</form>';
-*/
-?>
+$displayCats = new Categories();
+$displayCats->showAllCats();
