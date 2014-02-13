e90fc2c955aa6153788dff402b4c1150872300ac
Lee <keitellf@gmail.com>
2014-02-12 21:30:54 -0600

    Original management design

--- scripts/catManage.js
+++ scripts/catManage.js
@@ -1,15 +1,37 @@
-function showCats(mD, parent) {
+function ajax(address, data, statechange) {
     window.XMLHttpRequest ? xmlhttp=new XMLHttpRequest() : xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
       
-    xmlhttp.onreadystatechange=function()
-      {
-          if (xmlhttp.readyState===4 && xmlhttp.status===200)
-            {                
-                document.getElementById("workArea").innerHTML=xmlhttp.responseText;
-            }
-      }
+    xmlhttp.onreadystatechange=statechange;
       
-    xmlhttp.open("POST",'scripts/categories.php',true);
+    xmlhttp.open("POST",address,true);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.send("maxdepth=" + mD + "&parent=" + parent);
+    xmlhttp.send(data);
+}
+
+function showCats(mD, parent) {
+	address     = 'scripts/categories.php';
+	data        = '';
+	statechange = function()
+    {
+        if (xmlhttp.readyState===4 && xmlhttp.status===200)
+          {                
+              document.getElementById("workArea").innerHTML=xmlhttp.responseText;
+          }
+    }
+	
+	ajax(address, data, statechange);
+}
+
+function deleteMe() {
+	address     = 'scripts/categories.php';
+	data        = 'action=delete&item=' + currentCats;
+	statechange = function()
+    {
+        if (xmlhttp.readyState===4 && xmlhttp.status===200)
+          {                
+              document.getElementById("message").innerHTML=xmlhttp.responseText;
+          }
+    }
+	
+	ajax(address, data, statechange);
 }
