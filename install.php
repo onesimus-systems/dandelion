d1455319d380ab82d7f8e9905f72d346df1f903f
Lee <keitellf@gmail.com>
2014-02-15 12:13:29 -0600

    Added configuration security and verifies ability to write

--- install.php
+++ install.php
@@ -4,6 +4,10 @@ if (file_exists('config/config.php')) {
     
     if (!$CONFIG['installed']) {
         $needToInstall = true;
+        
+        if (!is_writable('config/')) { // Is it possible to write the config file?
+            $message = 'Dandelion does not have sufficient write permissions to create configuration.<br />Please make the ./config directory writeable to Dandelion and try again.';
+        }
     }
     else {
         $needToInstall = false;
@@ -11,6 +15,9 @@ if (file_exists('config/config.php')) {
 }
 else {
     $needToInstall = true;
+    if (!is_writable('config/')) { // Is it possible to write the config file?
+        $message = 'Dandelion does not have sufficient write permissions to create configuration.<br />Please make the ./config directory writeable to Dandelion and try again.';
+    }
 }
 
 if ($needToInstall) {
@@ -29,31 +36,37 @@ if ($needToInstall) {
             
             <p>The following information is needed to install Dandelion:</p>
             
+            <?php
+            if (!is_null($message)) {
+                echo $message;
+            }
+            else {?>
             <form method="post" class="le" action="install/install.php">
-                    <h2>Database Information</h2>
-                    
-                    <table>
-                        <tr>
-                            <td>Username:</td>
-                            <td><input type="text" name="dbuname" /></td>
-                        </tr>
-                        <tr>
-                            <td>Password:</td>
-                            <td><input type="password" name="dbpass" /></td>
-                        </tr>
-                        <tr>
-                            <td>Host/IP Address:</td>
-                            <td><input type="text" name="dbhost" /></td>
-                        </tr>
-                        <tr>
-                            <td>Database Name:</td>
-                            <td><input type="text" name="dbname" /></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Finish Install" /></td>
-                        </tr>
-                    </table>
+                <h2>Database Information</h2>
+                
+                <table>
+                    <tr>
+                        <td>Username:</td>
+                        <td><input type="text" name="dbuname" /></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input type="password" name="dbpass" /></td>
+                    </tr>
+                    <tr>
+                        <td>Host/IP Address:</td>
+                        <td><input type="text" name="dbhost" /></td>
+                    </tr>
+                    <tr>
+                        <td>Database Name:</td>
+                        <td><input type="text" name="dbname" /></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Finish Install" /></td>
+                    </tr>
+                </table>
             </form>
+            <?php } ?>
         </body>
     </html>
 <?php
--- install/install.php
+++ install/install.php
@@ -9,134 +9,142 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	);
 	
 	try {
-		$conn = new PDO('mysql:host='.$CONFIG['db_host'].';dbname='.$CONFIG['db_name'], $CONFIG['db_user'], $CONFIG['db_pass'], array(
-			PDO::ATTR_PERSISTENT => true
-		));
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$dbConn = $conn;
-		
-		/** Drop any existing tables in the database */
-		$stmt = 'SHOW TABLES';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		$allTables = $exec->fetchAll();
-		
-		if ($allTables[0]) {
-			$drop = 'DROP TABLES ';
-			
-			foreach ($allTables as $table) {
-				$drop .= '`'.$table[0].'`,';
-			}
-			
-			$drop = rtrim($drop, ',');
-			$drop .= ';';
-			
-			$exec = $dbConn->prepare($drop);
-			$exec->execute();
-		}
+        if (is_writable('../config')) { // Is it possible to write the config file?
+            $conn = new PDO('mysql:host='.$CONFIG['db_host'].';dbname='.$CONFIG['db_name'], $CONFIG['db_user'], $CONFIG['db_pass'], array(
+                PDO::ATTR_PERSISTENT => true
+            ));
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbConn = $conn;
+            
+            /** Drop any existing tables in the database */
+            $stmt = 'SHOW TABLES';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            $allTables = $exec->fetchAll();
+            
+            if ($allTables[0]) {
+                $drop = 'DROP TABLES ';
+                
+                foreach ($allTables as $table) {
+                    $drop .= '`'.$table[0].'`,';
+                }
+                
+                $drop = rtrim($drop, ',');
+                $drop .= ';';
+                
+                $exec = $dbConn->prepare($drop);
+                $exec->execute();
+            }
 
-		/** Create category table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `category` (
-				  `cid` int(11) NOT NULL AUTO_INCREMENT,
-				  `desc` varchar(255) NOT NULL,
-				  `ptree` varchar(11) NOT NULL,
-				  PRIMARY KEY (`cid`)
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
+            /** Create category table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `category` (
+                      `cid` int(11) NOT NULL AUTO_INCREMENT,
+                      `desc` varchar(255) NOT NULL,
+                      `ptree` varchar(11) NOT NULL,
+                      PRIMARY KEY (`cid`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create log table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `log` (
+                      `logid` int(20) NOT NULL AUTO_INCREMENT,
+                      `datec` date NOT NULL,
+                      `timec` time NOT NULL,
+                      `title` varchar(300) NOT NULL,
+                      `entry` longtext NOT NULL,
+                      `usercreated` varchar(255) NOT NULL,
+                      `cat` varchar(3000) NOT NULL,
+                      `edited` tinyint(1) NOT NULL DEFAULT \'0\',
+                      PRIMARY KEY (`logid`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create presence (Cxeesto) table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `presence` (
+                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                      `uid` int(11) NOT NULL,
+                      `realname` text NOT NULL,
+                      `status` tinyint(2) NOT NULL,
+                      `message` text NOT NULL,
+                      `return` text NOT NULL,
+                      `dmodified` datetime NOT NULL,
+                      PRIMARY KEY (`id`),
+                      UNIQUE KEY `User_ID` (`uid`)
+                    ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create session_token table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `session_token` (
+                      `session_id` int(255) NOT NULL AUTO_INCREMENT,
+                      `token` varchar(256) NOT NULL,
+                      `userid` int(10) NOT NULL,
+                      `expire` int(255) NOT NULL,
+                      PRIMARY KEY (`session_id`)
+                    ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create settings table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `settings` (
+                      `settings_id` int(255) NOT NULL AUTO_INCREMENT,
+                      `message` varchar(1000) NOT NULL,
+                      PRIMARY KEY (`settings_id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create users table */
+            $stmt = 'CREATE TABLE IF NOT EXISTS `users` (
+                      `userid` int(255) NOT NULL AUTO_INCREMENT,
+                      `username` varchar(255) NOT NULL,
+                      `password` varchar(255) NOT NULL,
+                      `realname` varchar(255) NOT NULL,
+                      `settings_id` int(10) NOT NULL,
+                      `role` varchar(255) NOT NULL,
+                      `datecreated` date NOT NULL,
+                      `firsttime` tinyint(1) NOT NULL DEFAULT \'2\',
+                      `showlimit` int(3) NOT NULL DEFAULT \'25\',
+                      PRIMARY KEY (`userid`)
+                    ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            /** Create admin user */
+            $stmt = 'INSERT INTO `presence` (`id`, `uid`, `realname`, `status`, `message`, `return`, `dmodified`)
+                    VALUES (1, 1, \'Admin\', 1, \'\', \'00:00:00\', \'2014-02-08 10:21:34\')';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            $stmt = 'INSERT INTO `users` (`userid`, `username`, `password`, `realname`,
+                        `settings_id`, `role`, `datecreated`, `firsttime`, `showlimit`)
+                        VALUES (1, \'admin\', \'$2y$10$sRDlu.F6gPVM4kS/k7ESHO9PF0Z5pXk0J/SpuMa88E31/Lux1mfMy\',
+                        \'Admin\', 0, \'admin\', \'2014-02-08\', 2, 25)';
+            $exec = $dbConn->prepare($stmt);
+            $exec->execute();
+            
+            $handle = fopen('../config/config.php', 'w');
+            $newFile = "<?php\n";
+            $newFile .= "\$CONFIG=array(\n";
+            $newFile .= "'db_name' => '".$CONFIG['db_name']."',\n";
+            $newFile .= "'db_host' => '".$CONFIG['db_host']."',\n";
+            $newFile .= "'db_user' => '".$CONFIG['db_user']."',\n";
+            $newFile .= "'db_pass' => '".$CONFIG['db_pass']."',\n";
+            $newFile .= "'installed' => true\n";
+            $newFile .= ");";
+            fwrite($handle, $newFile);
+            
+            // Change config directory to user:readonly for security
+            chmod('../config/config.php', 0400);
+            chmod('../config', 0500);
 		
-		/** Create log table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `log` (
-				  `logid` int(20) NOT NULL AUTO_INCREMENT,
-				  `datec` date NOT NULL,
-				  `timec` time NOT NULL,
-				  `title` varchar(300) NOT NULL,
-				  `entry` longtext NOT NULL,
-				  `usercreated` varchar(255) NOT NULL,
-				  `cat` varchar(3000) NOT NULL,
-				  `edited` tinyint(1) NOT NULL DEFAULT \'0\',
-				  PRIMARY KEY (`logid`)
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		/** Create presence (Cxeesto) table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `presence` (
-				  `id` int(11) NOT NULL AUTO_INCREMENT,
-				  `uid` int(11) NOT NULL,
-				  `realname` text NOT NULL,
-				  `status` tinyint(2) NOT NULL,
-				  `message` text NOT NULL,
-				  `return` text NOT NULL,
-				  `dmodified` datetime NOT NULL,
-				  PRIMARY KEY (`id`),
-				  UNIQUE KEY `User_ID` (`uid`)
-				) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		/** Create session_token table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `session_token` (
-				  `session_id` int(255) NOT NULL AUTO_INCREMENT,
-				  `token` varchar(256) NOT NULL,
-				  `userid` int(10) NOT NULL,
-				  `expire` int(255) NOT NULL,
-				  PRIMARY KEY (`session_id`)
-				) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		/** Create settings table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `settings` (
-				  `settings_id` int(255) NOT NULL AUTO_INCREMENT,
-				  `message` varchar(1000) NOT NULL,
-				  PRIMARY KEY (`settings_id`)
-				) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		/** Create users table */
-		$stmt = 'CREATE TABLE IF NOT EXISTS `users` (
-				  `userid` int(255) NOT NULL AUTO_INCREMENT,
-				  `username` varchar(255) NOT NULL,
-				  `password` varchar(255) NOT NULL,
-				  `realname` varchar(255) NOT NULL,
-				  `settings_id` int(10) NOT NULL,
-				  `role` varchar(255) NOT NULL,
-				  `datecreated` date NOT NULL,
-				  `firsttime` tinyint(1) NOT NULL DEFAULT \'2\',
-				  `showlimit` int(3) NOT NULL DEFAULT \'25\',
-				  PRIMARY KEY (`userid`)
-				) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		/** Create admin user */
-		$stmt = 'INSERT INTO `presence` (`id`, `uid`, `realname`, `status`, `message`, `return`, `dmodified`)
-				VALUES (1, 1, \'Admin\', 1, \'\', \'00:00:00\', \'2014-02-08 10:21:34\')';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		$stmt = 'INSERT INTO `users` (`userid`, `username`, `password`, `realname`,
-					`settings_id`, `role`, `datecreated`, `firsttime`, `showlimit`)
-					VALUES (1, \'admin\', \'$2y$10$sRDlu.F6gPVM4kS/k7ESHO9PF0Z5pXk0J/SpuMa88E31/Lux1mfMy\',
-					\'Admin\', 0, \'admin\', \'2014-02-08\', 2, 25)';
-		$exec = $dbConn->prepare($stmt);
-		$exec->execute();
-		
-		$handle = fopen('../config/config.php', 'w');
-		$newFile = "<?php\n";
-		$newFile .= "\$CONFIG=array(\n";
-		$newFile .= "'db_name' => '".$CONFIG['db_name']."',\n";
-		$newFile .= "'db_host' => '".$CONFIG['db_host']."',\n";
-		$newFile .= "'db_user' => '".$CONFIG['db_user']."',\n";
-		$newFile .= "'db_pass' => '".$CONFIG['db_pass']."',\n";
-		$newFile .= "'installed' => true\n";
-		$newFile .= ");";
-		
-		fwrite($handle, $newFile);
-		
-		header( 'Location: ../index.php' );
+            header( 'Location: ../scripts/logout.php' );
+        }
+        else {
+            echo 'Dandelion does not have sufficient write permissions to create configuration.<br />Please make the ./config directory writeable to Dandelion and try again.';
+        }
 	}
 	catch(PDOException $e) {
 		echo 'Error setting up database.';
