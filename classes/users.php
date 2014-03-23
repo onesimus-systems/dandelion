96f5d541ec38eefa716117329096bbd8fc6b4a54
dragonrider23 <keitellf@gmail.com>
2014-03-23 16:13:16 -0500

    Finished refactor of user management

--- classes/users.php
+++ classes/users.php
@@ -3,121 +3,156 @@
   * @brief User handles all user management tasks
   *
   * This class can be used to add, edit, and delete a user
+  * 
+  * @param conn - database connection object
   *
   * @author Lee Keitel
   * @date March, 2014
+  * 
+  * @license GNU GPL v3 (see full license in root/LICENSE.md)
 ***/
 class User
-{        
+{
+	public $conn;
+	
+	public function __construct($conn) {
+		$this->conn = $conn;
+	}
+	
+	/** Update user information
+	 *
+	 * @param userInfo (keyed array) - User information in a associative array
+	 * 		realname, sid, role, first, uid, theme
+	 *
+	 * @return Success message
+	 */
 	public function editUser($userInfoArray) {
-		if (!empty($userInfoArray)) {
-			$stmt = 'UPDATE `users` SET `realname` = :realname, `settings_id` = :s_id, `role` = :role, `firsttime` = :first, `theme` = :theme WHERE `userid` = :userid';
-			$params = array(
-				'realname' => $userInfoArray['realname'],
-				's_id' => $userInfoArray['sid'],
-				'role' => $userInfoArray['role'],
-				'first' => $userInfoArray['first'],
-				'userid' => $userInfoArray['uid'],
-				'theme' => $userInfoArray['theme']
-			);
+		$stmt = 'UPDATE `users` SET `realname` = :realname, `settings_id` = :sid, `role` = :role, `firsttime` = :first, `theme` = :theme WHERE `userid` = :userid';
+		$params = array(
+			'realname' => $userInfoArray['realname'],
+			'sid' => $userInfoArray['sid'],
+			'role' => $userInfoArray['role'],
+			'first' => $userInfoArray['first'],
+			'userid' => $userInfoArray['uid'],
+			'theme' => $userInfoArray['theme']
+		);
 
-			$conn->queryDB($stmt, $params);
+		$this->conn->queryDB($stmt, $params);
 
-			$stmt = 'UPDATE `presence` SET `realname` = :realname WHERE `uid` = :userid';
-			$params = array(
-				'realname' => $userInfoArray['realname'],
-				'userid' => $userInfoArray['uid']
-			);
+		$stmt = 'UPDATE `presence` SET `realname` = :realname WHERE `uid` = :userid';
+		$params = array(
+			'realname' => $userInfoArray['realname'],
+			'userid' => $userInfoArray['uid']
+		);
 
-			$conn->queryDB($stmt, $params);
+		$this->conn->queryDB($stmt, $params);
 
-			return 'User Updated<br /><br />';
-		}
-		else {
-			return false;
-		}
+		return 'User Updated<br /><br />';
 	}
-        
+
+	/** Create a new user
+	 *
+	 * @param userInfo (keyed array) - User information in a associative array
+	 * 		username, password, realname, sid, role
+	 *
+	 * @return Success message
+	 */
 	public function addUser($userInfoArray) {
-		if (!empty($userInfoArray)) {
-			$stmt = 'SELECT * FROM `users` WHERE `username` = :username';
+		$stmt = 'SELECT * FROM `users` WHERE `username` = :username';
+		$params = array(
+			'username' => $userInfoArray['username']
+		);
+		$row = $this->conn->queryDB($stmt, $params);
+
+		if ($row == NULL) {
+			$date = new DateTime();
+			$add_user = $userInfoArray['username'];
+			$add_pass = password_hash($userInfoArray['password'], PASSWORD_BCRYPT);
+			$add_real = $userInfoArray['realname'];
+			$add_sid = $userInfoArray['sid'];
+			$add_role = $userInfoArray['role'];
+
+			$stmt = 'INSERT INTO users (username, password, realname, settings_id, role, datecreated, theme) VALUES (:username, :password, :realname, :s_id, :role, :datecreated, \'default\')';
 			$params = array(
-				'username' => $userInfoArray['username']
-			);
-			$row = $conn->queryDB($stmt, $params);
+				'username' => $add_user,
+				'password' => $add_pass,
+				'realname' => $add_real,
+				's_id' => $add_sid,
+				'role' => $add_role,
+				'datecreated' => $date->format('Y-m-d')
+			);    
+			$this->conn->queryDB($stmt, $params);
 
-			if ($row == NULL) {
-				$date = new DateTime();
-				$add_user = $userInfoArray['username'];
-				$add_pass = password_hash($userInfoArray['password'], PASSWORD_BCRYPT);
-				$add_real = $userInfoArray['realname'];
-				$add_sid = $userInfoArray['sid'];
-				$add_role = $userInfoArray['role'];
+			$lastID = $this->conn->lastInsertId();
 
-				$stmt = 'INSERT INTO users (username, password, realname, settings_id, role, datecreated, theme) VALUES (:username, :password, :realname, :s_id, :role, :datecreated, \'default\')';
-				$params = array(
-					'username' => $add_user,
-					'password' => $add_pass,
-					'realname' => $add_real,
-					's_id' => $add_sid,
-					'role' => $add_role,
-					'datecreated' => $date->format('Y-m-d')
-				);    
-				$conn->queryDB($stmt, $params);
+			$stmt = 'INSERT INTO `presence` (`uid`, `realname`, `status`, `message`, `return`, `dmodified`) VALUES (:uid, :real, 1, \'\', \'00:00:00\', :date)';
+			$params = array(
+				'uid' => $lastID,
+				'real' => $add_real,
+				'date' => $date->format('Y-m-d H:i:s')
+			);    
+			$this->conn->queryDB($stmt, $params);
 
-				$lastID = $conn->lastInsertId();
-
-				$stmt = 'INSERT INTO `presence` (`uid`, `realname`, `status`, `message`, `return`, `dmodified`) VALUES (:uid, :real, 1, \'\', \'00:00:00\', :date)';
-				$params = array(
-					'uid' => $lastID,
-					'real' => $add_real,
-					'date' => $date->format('Y-m-d H:i:s')
-				);    
-				$conn->queryDB($stmt, $params);
-
-				return 'User Added<br /><br />';
-			}
-
-			else {
-				return 'Username already exists!';
-			}
+			return 'User Added<br /><br />';
 		}
 		else {
-			return false;
+			return 'Username already exists!';
 		}
 	}
-        
-	public function resetUserPw($pass1, $pass2, $uid) {
-		if ($pass1 == $pass2 && $pass1 != '') {
-			$pass1 = password_hash($pass1, PASSWORD_BCRYPT);
 
+	/** Reset user password
+	 *
+	 * @param pass (string) - New password
+	 * @param uid (int) - User's id number
+	 *
+	 * @return Success message
+	 */
+	public function resetUserPw($pass, $uid) {
+		if (is_numeric($uid)) {
+			$pass = password_hash($pass, PASSWORD_BCRYPT);
+	
 			$stmt = 'UPDATE `users` SET `password` = :newpass WHERE `userid` = :myID';
 			$params = array(
-				'newpass' => $pass1,
+				'newpass' => $pass,
 				'myID' => $uid
 			);
-			$conn->queryDB($stmt, $params);
+			$this->conn->queryDB($stmt, $params);
 	        
 			return 'Password change successful.<br /><br />';
 		}
 		else {
-			return false;
+			return 'Error resetting password.<br><br>';
 		}
 	}
-        
+
+	/** Delete user
+	 *
+	 * @param uid (int) - User's id number
+	 *
+	 * @return Success message
+	 */
 	public function deleteUser($uid) {
 		$stmt = 'DELETE FROM `users` WHERE `userid` = :userid';
 		$stmt2 = 'DELETE FROM `presence` WHERE `uid` = :userid';
 		$params = array(
-			'userid' => $choosen
+			'userid' => $uid
 		);
 	
-		$conn->queryDB($stmt, $params);
-		$conn->queryDB($stmt2, $params);
+		$this->conn->queryDB($stmt, $params);
+		$this->conn->queryDB($stmt2, $params);
 	
-		echo "Action Taken: User Deleted<br /><br />";
+		return "Action Taken: User Deleted<br /><br />";
 	}
-        
+	
+	/** Update user status
+	 *
+	 * @param uid (int) - User's id number
+	 * @param status_id (int) - # for user status type
+	 * @param message (string) - User's away message
+	 * @param returntime (date/time) - Return time for away user
+	 *
+	 * @return Success message
+	 */
 	public function updateUserStatus($uid, $status_id, $message, $returntime) {
 		$date = new DateTime();
 		$date = $date->format('Y-m-d H:i:s');
@@ -125,7 +160,8 @@ class User
 		switch($status_id) {
 			case "Available":
 				$status_id = 1;
-				$returntime = "00:00:00";
+				$returntime = '00:00:00';
+				$message = '';
 				break;
 			case "Away From Desk":
 				$status_id = 2;
@@ -168,7 +204,7 @@ class User
 			'date' => $date,
 			'userid' => $uid
 		);
-		$conn->queryDB($stmt, $params);
+		$this->conn->queryDB($stmt, $params);
 		
 		return 'User Status Updated<br /><br />';
 	}
--- classes/usersForms.php
+++ classes/usersForms.php
@@ -4,119 +4,109 @@
   *
   * @author Lee Keitel
   * @date March, 2014
+  * 
+  * @license GNU GPL v3 (see full license in root/LICENSE.md)
 ***/
 class UserForms
-{
-	if ($u_action == "delete") { // Confirm user delete
-		
-		// Delete selected user from DB
-		if ($choosen != NULL AND $choosen != "") {
-		?>
-			<br /><hr width="500">
-			Are you sure you want to delete "<?php echo $edit_user_info['realname'] ?>"?<br /><br />
-			<form method="post">
-				<input type="hidden" name="the_choosen_one" value="<?php echo $choosen; ?>" />
-				<input type="submit" name="sub_type" value="Yes" />
-				<input type="submit" value="No" />
-			</form><hr width="500"><br />
-		<?php
-			}
-			else {
-				echo "ERROR: No user was selected to delete.<br /><br />";
-			}
+{	
+	/** Confirm user delete form
+	 *
+	 * @param name (string) - User's real name
+	 * @param uid (int) - User's ID number
+	 *
+	 * @return nothing
+	 */
+	public function confirmDelete($name, $uid) { 
+	?>
+		<br /><hr width="500">
+		Are you sure you want to delete "<?php echo $name; ?>"?<br /><br />
+		<form method="post">
+			<input type="hidden" name="the_choosen_one" value="<?php echo $uid; ?>" />
+			<input type="submit" name="sub_type" value="Yes" />
+			<input type="submit" value="No" />
+		</form><hr width="500"><br />
+	<?php
 	}
-	        
-	elseif ($u_action == "cxeesto") { // Show status update form
-	
-		if ($choosen != NULL AND $choosen != "") {
-			//Form to edit Cxeesto status.
-			$stmt = 'SELECT * FROM `presence` WHERE `uid` = :userid';
-			$params = array(
-				'userid' => $choosen
-			);
-			$row = $conn->queryDB($stmt, $params);
-			
-			if (!empty($row)) {
-				$row = $row[0];
-				?>
-				<div id="editform">
-					<h2>Edit User Status:</h2>
-					<form name="edit_form" method="post">
-						<table>
-							<tr><td>User ID:</td><td><input type="text" name="status_id" value="<?php echo $row['uid']; ?>" autocomplete="off" readonly /></td></tr>
-							<tr><td>Name:</td><td><input type="text" name="status_name" value="<?php echo $row['realname']; ?>" autocomplete="off" readonly /></td></tr>
-							<tr><td>Status:</td><td>
-								<select name="status_s">
-									<option>Set Status:</option>
-									<option>Available</option>
-									<option>Away From Desk</option>
-									<option>At Lunch</option>
-									<option>Out for Day</option>
-									<option>Out</option>
-									<option>Appointment</option>
-									<option>Do Not Disturb</option>
-									<option>Meeting</option>
-									<option>Out Sick</option>
-									<option>Vacation</option>
-								</select></td></tr>
-							<tr><td>Message:</td><td><textarea cols="30" rows="5" name="status_message"><?php echo $row['message']; ?></textarea></td></tr>
-							<tr><td>Return:</td><td><input type="text" name="status_return" id="datepick" value="<?php echo $row['return']; ?>" /> Format: MM/DD/YYYY 13:00</td></tr>
-						</table>
-						<input type="submit" name="sub_type" value="Set Status" />
-						<input type="submit" name="sub_type" value="Cancel" />
-					</form>
-				</div><br />
-				<?php
-				$showList = false;
-			}
-			else {
-				echo "ERROR: Selected user doesn't have a &#264;eesto account.<br /><br />";
-			}
-		}
-		else {
-			echo "ERROR: No user was selected to edit &#264;eesto status.<br /><br />";
-		}
+
+	/** Edit user status form
+	 *
+	 * @param row (associative array) - All user information from database for Cxeesto
+	 *
+	 * @return nothing
+	 */
+	public function editCxeesto($row) {
+	?>
+		<div id="editform">
+			<h2>Edit User Status:</h2>
+			<form name="edit_form" method="post">
+				<table>
+					<tr><td>User ID:</td><td><input type="text" name="status_id" value="<?php echo $row['uid']; ?>" autocomplete="off" readonly /></td></tr>
+					<tr><td>Name:</td><td><input type="text" name="status_name" value="<?php echo $row['realname']; ?>" autocomplete="off" readonly /></td></tr>
+					<tr><td>Status:</td><td>
+						<select name="status_s">
+							<option>Set Status:</option>
+							<option>Available</option>
+							<option>Away From Desk</option>
+							<option>At Lunch</option>
+							<option>Out for Day</option>
+							<option>Out</option>
+							<option>Appointment</option>
+							<option>Do Not Disturb</option>
+							<option>Meeting</option>
+							<option>Out Sick</option>
+							<option>Vacation</option>
+						</select></td></tr>
+					<tr><td>Message:</td><td><textarea cols="30" rows="5" name="status_message"><?php echo $row['message']; ?></textarea></td></tr>
+					<tr><td>Return:</td><td><input type="text" name="status_return" id="datepick" value="<?php echo $row['return']; ?>" /> Format: MM/DD/YYYY 13:00</td></tr>
+				</table>
+				<input type="submit" name="sub_type" value="Set Status" />
+				<input type="submit" name="sub_type" value="Cancel" />
+			</form>
+		</div><br />
+	<?php
 	}
-	        
-	elseif ($u_action == "edit") { // Show edit user form
-		// Form to edit user information
-		if ($choosen != NULL AND $choosen != "") {
-			?>
-			<div id="editform">
-				<h2>Edit User Information:</h2>
-				<form name="edit_form" method="post">
-					<table>
-						<tr><td>User ID:</td><td><input type="text" name="edit_uid" value="<?php echo $edit_user_info['userid']; ?>" readonly /></td></tr>
-						<tr><td>Real Name:</td><td><input type="text" name="edit_real" value="<?php echo $edit_user_info['realname']; ?>" autocomplete="off" /></td></tr>
-						<tr><td>Settings ID:</td><td><input type="text" name="edit_sid" value="<?php echo $edit_user_info['settings_id']; ?>" autocomplete="off" /></td></tr>
-						<tr><td>Role:</td><td>
-							<select name="edit_role">
-								<option value="user" <?php echo $edit_user_info['role'] == 'user' ? ' selected' : '';?>>User</option>
-								<option value="guest" <?php echo $edit_user_info['role'] == 'guest' ? ' selected' : '';?>>Guest</option>
-								<option value="admin" <?php echo $edit_user_info['role'] == 'admin' ? ' selected' : '';?>>Admin</option>
-							</select>
-						</td></tr>
-						<tr><td>Theme:</td><td>
-							<?php getThemeList($edit_user_info['theme']); ?>
-						</td></tr>
-						<tr><td>Date Created:</td><td><input type="text" name="edit_date" value="<?php echo $edit_user_info['datecreated']; ?>" readonly /></td></tr>
-						<tr><td>First Login:</td><td><input type="text" name="edit_first" value="<?php echo $edit_user_info['firsttime']; ?>" autocomplete="off" /></td></tr>
-					</table>
-					<input type="submit" name="sub_type" value="Save Edit" />
-					<input type="submit" name="sub_type" value="Cancel" />
-				</form>
-			</div><br />
-			<?php
-			$showList = false;
-		}
-		else {
-			echo "ERROR: No user was selected to edit.<br /><br />";
-		}
+
+	/** Edit user form
+	 *
+	 * @param userInfo (keyed array) - All user information from database
+	 *
+	 * @return nothing
+	 */
+	public function editUser($userInfo) {
+	?>
+		<div id="editform">
+			<h2>Edit User Information:</h2>
+			<form name="edit_form" method="post">
+				<table>
+					<tr><td>User ID:</td><td><input type="text" name="edit_uid" value="<?php echo $userInfo['userid']; ?>" readonly></td></tr>
+					<tr><td>Real Name:</td><td><input type="text" name="edit_real" value="<?php echo $userInfo['realname']; ?>" autocomplete="off"></td></tr>
+					<tr><td>Settings ID:</td><td><input type="text" name="edit_sid" value="<?php echo $userInfo['settings_id']; ?>" autocomplete="off"></td></tr>
+					<tr><td>Role:</td><td>
+						<select name="edit_role">
+							<option value="user" <?php echo $userInfo['role'] == 'user' ? ' selected' : '';?>>User</option>
+							<option value="guest" <?php echo $userInfo['role'] == 'guest' ? ' selected' : '';?>>Guest</option>
+							<option value="admin" <?php echo $userInfo['role'] == 'admin' ? ' selected' : '';?>>Admin</option>
+						</select>
+					</td></tr>
+					<tr><td>Theme:</td><td>
+						<?php getThemeList($userInfo['theme']); ?>
+					</td></tr>
+					<tr><td>Date Created:</td><td><input type="text" name="edit_date" value="<?php echo $userInfo['datecreated']; ?>" readonly></td></tr>
+					<tr><td>First Login:</td><td><input type="text" name="edit_first" value="<?php echo $userInfo['firsttime']; ?>" autocomplete="off"></td></tr>
+				</table>
+				<input type="submit" name="sub_type" value="Save Edit">
+				<input type="submit" name="sub_type" value="Cancel">
+			</form>
+		</div><br>
+	<?php
 	}
-	        
-	elseif ($u_action == "add") { // Show create user form		
-		// Form to add a new user
-		?>
+
+	/** Add new user form
+	 *
+	 * @return nothing
+	 */
+	public function addUser() {
+	?>
 		<div id="editform">
 			<h2>Add a User:</h2>
 				<form name="edit_form" method="post">
@@ -127,36 +117,36 @@ class UserForms
 						<tr><td>Settings ID:</td><td><input type="text" name="add_sid" value="0" autocomplete="off" readonly /></td></tr>
 						<tr><td>Role:</td><td><select name="add_role"><option value="user">User</option><option value="guest">Guest</option><option value="admin">Admin</option></select></td></tr>
 					</table>
-					<input type="submit" name="sub_type" value="Add" />
-					<input type="submit" name="sub_type" value="Cancel" />
+					<input type="submit" name="sub_type" value="Add">
+					<input type="submit" name="sub_type" value="Cancel">
 				</form>
-		</div><br />
-		<?php
-		$showList = false;
+		</div><br>
+	<?php
 	}
-	        
-	elseif ($u_action == "reset") { // Show password reset form
-		// Form to reset user's password
-		if ($choosen != NULL AND $choosen != "") {
-			?>
-			<div id="editform">
-				<h2>Reset Password for <?php echo $edit_user_info['realname'] ?>:</h2>
-				<form name="edit_form" method="post">
-					<table>
-						<tr><td>User ID:</td><td><input type="text" name="reset_uid" value="<?php echo $edit_user_info['userid']; ?>" readonly /></td></tr>
-						<tr><td>Username:</td><td><input type="text" value="<?php echo $edit_user_info['username']; ?>" readonly /></td></tr>
-						<tr><td>New Password:</td><td><input type="password" name="reset_1" /></td></tr>
-						<tr><td>Repeat Password:</td><td><input type="password" name="reset_2" /></td></tr>
-					</table>
-					<input type="submit" name="sub_type" value="Reset" />
-					<input type="submit" name="sub_type" value="Cancel" />
-				</form>
-			</div><br />
-			<?php
-			$showList = false;
-		}
-		else {
-			echo "ERROR: No user was selected to reset password.<br /><br />";
-		}
+
+	/** Confirm user delete form
+	 *
+	 * @param uid (int) - User's ID number
+	 * @param uname (string) - User's username
+	 * @param realname (string) - User's name
+	 *
+	 * @return nothing
+	 */
+	public function resetPassword($uid, $uname, $realname) {
+	?>
+		<div id="editform">
+			<h2>Reset Password for <?php echo $realname; ?>:</h2>
+			<form name="edit_form" method="post">
+				<table>
+					<tr><td>User ID:</td><td><input type="text" name="reset_uid" value="<?php echo $uid; ?>" readonly /></td></tr>
+					<tr><td>Username:</td><td><input type="text" value="<?php echo $uname; ?>" readonly /></td></tr>
+					<tr><td>New Password:</td><td><input type="password" name="reset_1" /></td></tr>
+					<tr><td>Repeat Password:</td><td><input type="password" name="reset_2" /></td></tr>
+				</table>
+				<input type="submit" name="sub_type" value="Reset" />
+				<input type="submit" name="sub_type" value="Cancel" />
+			</form>
+		</div><br />
+	<?php
 	}
 }
