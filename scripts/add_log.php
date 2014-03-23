a532268d7f44d6a9d1779d933338f33467ff5baa
dragonrider23 <keitellf@gmail.com>
2014-03-23 17:07:23 -0500

    Created blacklist

--- scripts/add_log.php
+++ scripts/add_log.php
@@ -1,51 +1,35 @@
 <?php
-/*
- * Lee Keitel
- * January 28, 2014
- *
+/**
  * This script is called via AJAX to create a new log entry.
- * 
-*/
+ *
+ * This file is a part of Dandelion
+ *
+ * @author Lee Keitel
+ * @date January 28, 2014
+ *
+ * @license GNU GPL v3 (see full license in root/LICENSE.md)
+ ***/
 
 include 'grabber.php';
 
-if (authenticated()) {
-	if ($_SESSION['userInfo']['role'] == "guest") {
-		header( 'Location: viewlog.phtml' );
-	}
-	
-	if ($_SESSION['userInfo']['role'] === "admin") {
-		$admin_link = '| <a href="admin.phtml">Administration</a>';
-	}
-	else {
-		$admin_link = '';
-	}
-	
-	if ($_SESSION['userInfo']['role'] !== "guest") {
-		$settings_link = '| <a href="settings.phtml">Settings</a>';
-	}
-	else {
-		$settings_link = '';
-	}
-}
-else {
+if (!authenticated()) {
 	header( 'Location: index.php' );
 }
 
-if ($_SESSION['userInfo']['username'] != 'ajmartin') { // This is the skeleton of an eventual blacklist/rights management
+$blacklist = array('ajmartin');
+
+if (in_array($_SESSION['userInfo']['username'], $blacklist)) { // This is the skeleton of an eventual blacklist/rights management
     // Grab all the variables from the POST array
     $new_title = isset($_POST['add_title']) ? $_POST['add_title'] : '';
     $new_entry = isset($_POST['add_entry']) ? $_POST['add_entry'] : '';
     $new_category = isset($_POST['cat']) ? $_POST['cat'] : '';
 
     // Check that all required fields have been entered
-    if ($new_title != NULL AND $new_title != "" AND $new_entry != NULL AND $new_entry != "" AND $new_category != NULL AND $new_category != "select") {
-        // Grab and format creation date/time
+    if ($new_title != NULL AND $new_title != "" AND $new_entry != NULL AND $new_entry != "" AND $new_category != NULL AND $new_category != "Select:") {
         $datetime = getdate();
         $new_date = $datetime['year'] . '-' . $datetime['mon'] . '-' . $datetime['mday'];
         $new_time = $datetime['hours'] . ':' . $datetime['minutes'] . ':' . $datetime['seconds'];
-       
-        // Connect to DB
+
         $conn = new dbManage();
         
         // Add new entry
@@ -55,7 +39,7 @@ if ($_SESSION['userInfo']['username'] != 'ajmartin') { // This is the skeleton o
             'timec' => $new_time,
             'title' => $new_title,
             'entry' => $new_entry,
-            'usercreated' => $_SESSION['userInfo']['userid'], // Don't ask
+            'usercreated' => $_SESSION['userInfo']['userid'],
             'cat' => $new_category,
         );
         $conn->queryDB($stmt, $params);
