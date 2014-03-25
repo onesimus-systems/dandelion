e6847f78bc6b3b70693b2d6f269812a80cd058a5
dragonrider23 <keitellf@gmail.com>
2014-03-24 19:27:44 -0500

    Added checks for updating log entry, removed unnecassary authentication code

--- scripts/editlogs.php
+++ scripts/editlogs.php
@@ -1,41 +1,22 @@
 <?php
-include 'grabber.php';
+require_once 'grabber.php';
 
-if (authenticated()) {
-	if ($_SESSION['userInfo']['role'] == "guest") {
-		header( 'Location: viewlog.phtml' );
-	}
-	
-	if ($_SESSION['userInfo']['role'] === "admin") {
-		$admin_link = '| <a href="admin.phtml">Administration</a>';
-	}
-	else {
-		$admin_link = '';
-	}
-	
-	if ($_SESSION['userInfo']['role'] !== "guest") {
-		$settings_link = '| <a href="settings.phtml">Settings</a>';
-	}
-	else {
-		$settings_link = '';
-	}
-}
-else {
+if (!authenticated()) {
 	header( 'Location: index.php' );
 }
 
 $editedlog = isset($_POST['editlog']) ? $_POST['editlog'] : '';
 $editedtitle = isset($_POST['edittitle']) ? $_POST['edittitle'] : '';
-$choosen  = isset($_POST['choosen']) ? $_POST['choosen'] : '';
+$logid  = isset($_POST['choosen']) ? $_POST['choosen'] : '';
 
-// Connect to DB
-$conn = new dbManage();
+if (!empty($editedlog) && !empty($editedtitle) && !empty($logid)) {
+	$conn = new dbManage();
 
-// Update the database
-$stmt = 'UPDATE `log` SET `title` = :eTitle, `entry` = :eEntry, `edited` = 1 WHERE `logid` = :leLogID';
-$params = array(
-    'eTitle' => $editedtitle,
-    'eEntry' => $editedlog,
-    'leLogID' => $choosen
-);
-$conn->queryDB($stmt, $params);
+	$stmt = 'UPDATE `log` SET `title` = :eTitle, `entry` = :eEntry, `edited` = 1 WHERE `logid` = :logid';
+	$params = array(
+	    'eTitle' => $editedtitle,
+	    'eEntry' => $editedlog,
+	    'logid' => $logid
+	);
+	$conn->queryDB($stmt, $params);
+}
