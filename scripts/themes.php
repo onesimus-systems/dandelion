b49e8ac33cb5818d51965cca6f2459d4932ef5b3
dragonrider23 <keitellf@gmail.com>
2014-03-24 21:30:48 -0500

    Pretty

--- scripts/themes.php
+++ scripts/themes.php
@@ -11,24 +11,21 @@
 ***/
 
 function getTheme() {
-	// If a theme is defined for the user in the database, grab it
 	$theme = !empty($_SESSION['userInfo']['theme']) ? $_SESSION['userInfo']['theme'] : 'default';
-	
-	// Check to see if the theme exists, if it doesn't fallback to default
 	$theme = is_dir(THEME_DIR.'/'.$theme) ? $theme : 'default';
 	return $theme;
 }
 
 function getThemeList($theme = null) {
-	// The call can pass the theme to use as selected,
+	// The call can pass the theme currently used,
 	// if one isn't passed, assume the current user's theme
-	$theme = ($theme===null ? getTheme() : $theme);
+	$currentTheme = ($theme===null) ? getTheme() : $theme;
 	$handle = opendir('themes');
 	
 	echo '<select name="userTheme">';
 	while (false !== ($themeName = readdir($handle))) {
 		if ($themeName != '.' && $themeName != '..' && is_dir(THEME_DIR.'/'.$themeName)) {
-			if ($themeName == $theme) {
+			if ($themeName == $currentTheme) {
 				echo '<option value="'.$themeName.'" selected>'.$themeName.'</option>';
 			}
 			else {
