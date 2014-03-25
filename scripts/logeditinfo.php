678288f098ae6f441bcfd67eb86eadc0881314c8
dragonrider23 <keitellf@gmail.com>
2014-03-24 21:01:57 -0500

    Remove unnecassary code

--- scripts/logeditinfo.php
+++ scripts/logeditinfo.php
@@ -15,39 +15,15 @@
 include 'grabber.php';
 
 if (authenticated()) {
-	if ($_SESSION['userInfo']['role'] == "guest") {
-		header( 'Location: viewlog.phtml' );
-	}
-	
-	if ($_SESSION['userInfo']['role'] === "admin") {
-		$admin_link = '| <a href="admin.phtml">Administration</a>';
-	}
-	else {
-		$admin_link = '';
-	}
-	
-	if ($_SESSION['userInfo']['role'] !== "guest") {
-		$settings_link = '| <a href="settings.phtml">Settings</a>';
-	}
-	else {
-		$settings_link = '';
-	}
+	$loguid = isset($_POST['loguid']) ? $_POST['loguid'] : '';
+
+	$conn = new dbManage();
+
+	$stmt = 'SELECT * FROM `log` WHERE `logid` = :logid';
+	$params = array(
+	    'logid' => $loguid
+	);
+	$edit_log_info = $conn->queryDB($stmt, $params);
+
+	echo json_encode($edit_log_info);
 }
-else {
-	header( 'Location: index.php' );
-}
-
-$loguid = isset($_POST['loguid']) ? $_POST['loguid'] : '';
-
-// Connect to DB
-$conn = new dbManage();
-
-// Grab log
-$stmt = 'SELECT * FROM `log` WHERE `logid` = :logid';
-$params = array(
-    'logid' => $loguid
-);
-$edit_log_info = $conn->queryDB($stmt, $params);
-
-// Return JSON encoded array with log data
-echo json_encode($edit_log_info);
