c19b32fe56294a3b3c150d66046227075cf9e500
dragonrider23 <keitellf@outlook.com>
2014-05-11 20:27:30 -0500

    Upgraded jQuery, added dev versions of jQuery Core and UI

--- scripts/scripts.php
+++ scripts/scripts.php
@@ -32,10 +32,10 @@ function loadJS() {
 		
 		switch($file) {
 			case "jquery":
-				echo '<script src="jquery/js/jquery.min.js"></script>';
+				echo '<script src="jquery/js/jquery-2.1.1.min.js"></script>';
 				break;
 			case "jqueryui":
-				echo '<script src="jquery/js/jquery-ui.min.js"></script>';
+				echo '<script src="jquery/js/jquery-ui-1.10.4.min.js"></script>';
 				break;
 			case "lquery":
 				echo '<script src="js/lQuery.js"></script>';
