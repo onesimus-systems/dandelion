d2ed4fcdd47ef69c07e6da414c69910716ce6168
dragonrider23_ <keitell@gmail.com>
2014-05-14 12:47:39 -0500

    Restructure and PHP version check

--- scripts/grabber.php
+++ scripts/grabber.php
@@ -11,7 +11,7 @@
 ***/
 
 /* 
- * This block checks the time of the current session and if it has been
+ * This checks the time of the current session and if it has been
  * in active for $timeout seconds long, destroy the session and start again
  */
 $timeout = 21600; // 6 hours
@@ -28,25 +28,8 @@ if (isset($_SESSION['timeout_idle']) && $_SESSION['timeout_idle'] < time()) {
 
 $_SESSION['timeout_idle'] = time() + $timeout;
 
-// Define constants
-if (!defined('D_VERSION')) {
-	define('D_VERSION', '4.6.0');     			// Defines current Dandelion version
-}
-if (!defined('THEME_DIR')) {
-	define('THEME_DIR', 'themes');	 			// Defines theme directory
-}
-if (!defined('ROOT')) {
-	define('ROOT', dirname(dirname(__FILE__)));	// Defines root path of application
-}
-
-// Load other scripts
-require_once ROOT.'/classes/db_functions.php';
-require_once ROOT.'/classes/permissions.php';
-require_once ROOT.'/scripts/authenticate.php';
-require_once ROOT.'/scripts/themes.php';
-require_once ROOT.'/scripts/logging.php';
-require_once ROOT.'/scripts/scripts.php';
-require_once ROOT.'/scripts/password_compat/password.php';
+// Get and define root path of application
+define('ROOT', dirname(dirname(__FILE__)));
 
 // Utilized in development to, well, force the config to reload without relogin.
 $forceConfigLoad = true;
@@ -55,28 +38,50 @@ $forceConfigLoad = true;
 if(!isset($_SESSION['config']) || $forceConfigLoad) {
 	if (file_exists(ROOT . '/config/config.php')) {
 		include ROOT.'/config/config.php';
-		$_SESSION['config'] = $CONFIG;
+		
+		if (isset($CONFIG)) {
+			$_SESSION['config'] = $CONFIG;
+		}
+		else {
+			trigger_error('The configuration file is corrupt or otherwise damaged. Please check config.php and try again.', E_USER_ERROR);
+			echo 'The configuration file is corrupt or otherwise damaged. Please check config.php and try again.';
+			exit(1);
+		}
 	}
 	else {
 		trigger_error('No configuration file found. Please create ROOT/config/config.php.', E_USER_ERROR);
 		echo 'No configuration file found.  Please create ROOT/config/config.php or <a href="./install">Start the Installer</a>.';
-		exit;
+		exit(1);
 	}
-}
-
-if (!defined('DB_PREFIX')) {
-	define('DB_PREFIX', $_SESSION['config']['db_prefix']);
-}
-
-if (!defined('HOSTNAME')) {
-	define('HOSTNAME', $_SESSION['config']['hostname']);
 }
 
 // Display errors if in debug mode
 error_reporting(E_ALL);
 if ($_SESSION['config']['debug']) {
 	ini_set('display_errors', True);
+	require_once ROOT.'/scripts/logging.php';
 }
+
+// The password compatibility library requires PHP version 5.3.7 or above
+if (version_compare(phpversion(), "5.3.7", "<")) {
+	trigger_error('Dandelion needs at least PHP version 5.3.7 to function.', E_USER_ERROR);
+	echo 'Dandelion needs at least PHP version 5.3.7 to function. You have version '.phpversion().' installed.';
+	exit(1);
+}
+
+// Define constants
+define('DB_PREFIX', $_SESSION['config']['db_prefix']);
+define('HOSTNAME', $_SESSION['config']['hostname']);
+define('D_VERSION', '4.6.0');
+define('THEME_DIR', 'themes');
+
+// Load other scripts
+require_once ROOT.'/classes/db_functions.php';
+require_once ROOT.'/classes/permissions.php';
+require_once ROOT.'/scripts/authenticate.php';
+require_once ROOT.'/scripts/themes.php';
+require_once ROOT.'/scripts/scripts.php';
+require_once ROOT.'/scripts/password_compat/password.php';
 
 // Load application settings
 if(!isset($_SESSION['app_settings'])) {
