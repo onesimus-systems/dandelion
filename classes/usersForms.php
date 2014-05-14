38feb9c7ff44171c3c4f714f8d8102487f1ef4ed
dragonrider23_ <keitell@gmail.com>
2014-05-14 13:09:46 -0500

    Use HEREDOC instead of mashedup HTML/PHP

--- classes/usersForms.php
+++ classes/usersForms.php
@@ -18,7 +18,7 @@ class UserForms
 	 */
 	public function confirmDelete($name, $uid) {
 		if ($uid != $_SESSION['userInfo']['userid']) {
-    	?>
+    		echo <<<HTML
     		<br /><hr width="500">
     		Are you sure you want to delete "<?php echo $name; ?>"?<br /><br />
     		<form method="post">
@@ -27,7 +27,7 @@ class UserForms
     			<input type="submit" value="No" />
     		</form>
     		<hr width="500"><br />
-    	<?php
+HTML;
 		}
 		else {
 			echo '<br>You can\'t delete yourself.<br><br>';
@@ -41,13 +41,13 @@ class UserForms
 	 * @return nothing
 	 */
 	public function editCxeesto($row) {
-	?>
+		echo <<<HTML
 		<div id="editform">
 			<h2>Edit User Status:</h2>
 			<form name="edit_form" method="post">
 				<table>
-					<tr><td>User ID:</td><td><input type="text" name="status_id" value="<?php echo $row['uid']; ?>" autocomplete="off" readonly /></td></tr>
-					<tr><td>Name:</td><td><input type="text" name="status_name" value="<?php echo $row['realname']; ?>" autocomplete="off" readonly /></td></tr>
+					<tr><td>User ID:</td><td><input type="text" name="status_id" value="{$row['uid']}" autocomplete="off" readonly /></td></tr>
+					<tr><td>Name:</td><td><input type="text" name="status_name" value="{$row['realname']}" autocomplete="off" readonly /></td></tr>
 					<tr><td>Status:</td><td>
 						<select name="status_s">
 							<option>Set Status:</option>
@@ -62,8 +62,8 @@ class UserForms
 							<option>Out Sick</option>
 							<option>Vacation</option>
 						</select></td></tr>
-					<tr><td>Message:</td><td><textarea cols="30" rows="5" name="status_message"><?php echo $row['message']; ?></textarea></td></tr>
-					<tr><td>Return:</td><td><input type="text" name="status_return" id="datepick" value="<?php echo $row['return']; ?>" /></td></tr>
+					<tr><td>Message:</td><td><textarea cols="30" rows="5" name="status_message">{$row['message']}</textarea></td></tr>
+					<tr><td>Return:</td><td><input type="text" name="status_return" id="datepick" value="{$row['return']}" /></td></tr>
 				</table>
 				<input type="submit" name="sub_type" value="Set Status" />
 				<input type="submit" name="sub_type" value="Cancel" />
@@ -79,7 +79,7 @@ class UserForms
             	});
 			</script>
 		</div><br />
-	<?php
+HTML;
 	}
 
 	/** Edit user form
@@ -89,19 +89,20 @@ class UserForms
 	 * @return nothing
 	 */
 	public function editUser($userInfo) {
-	?>
+		$permissions = new Permissions();
+		$list = $permissions->getGroupList();
+		$themeList = getThemeList($userInfo['theme']);
+	
+		echo <<<HTML
 		<div id="editform">
 			<h2>Edit User Information:</h2>
 			<form name="edit_form" method="post">
 				<table>
-					<tr><td>User ID:</td><td><input type="text" name="edit_uid" value="<?php echo $userInfo['userid']; ?>" readonly></td></tr>
-					<tr><td>Real Name:</td><td><input type="text" name="edit_real" value="<?php echo $userInfo['realname']; ?>" autocomplete="off"></td></tr>
+					<tr><td>User ID:</td><td><input type="text" name="edit_uid" value="{$userInfo['userid']}" readonly></td></tr>
+					<tr><td>Real Name:</td><td><input type="text" name="edit_real" value="{$userInfo['realname']}" autocomplete="off"></td></tr>
 					<tr><td>Role:</td><td>
-					<?php
-                        $permissions = new Permissions();
-                        $list = $permissions->getGroupList();
-                        
-                        echo '<select name="edit_role">';
+					<select name="edit_role">
+HTML;
                         foreach ($list as $group) {
                             if ($group['role'] == $userInfo['role'])
                                 $selected = 'selected';
@@ -110,20 +111,20 @@ class UserForms
                                 
                             echo '<option value="'.$group['role'].'" '.$selected.'>'.ucfirst($group['role']).'</option>';
                         }
-                        echo '</select>';
-						?>
+		echo <<<HTML
+					</select>
 					</td></tr>
 					<tr><td>Theme:</td><td>
-						<?php getThemeList($userInfo['theme']); ?>
+						{$themeList}
 					</td></tr>
-					<tr><td>Date Created:</td><td><input type="text" name="edit_date" value="<?php echo $userInfo['datecreated']; ?>" readonly></td></tr>
-					<tr><td>First Login:</td><td><input type="text" name="edit_first" value="<?php echo $userInfo['firsttime']; ?>" autocomplete="off"></td></tr>
+					<tr><td>Date Created:</td><td><input type="text" name="edit_date" value="{$userInfo['datecreated']}" readonly></td></tr>
+					<tr><td>First Login:</td><td><input type="text" name="edit_first" value="{$userInfo['firsttime']}" autocomplete="off"></td></tr>
 				</table>
 				<input type="submit" name="sub_type" value="Save Edit">
 				<input type="submit" name="sub_type" value="Cancel">
 			</form>
 		</div><br>
-	<?php
+HTML;
 	}
 
 	/** Add new user form
@@ -131,7 +132,9 @@ class UserForms
 	 * @return nothing
 	 */
 	public function addUser() {
-	?>
+		$permissions = new Permissions();
+		$list = $permissions->getGroupList();
+		echo <<<HTML
 		<div id="editform">
 			<h2>Add a User:</h2>
 				<form name="edit_form" method="post">
@@ -140,23 +143,20 @@ class UserForms
 						<tr><td>Password:</td><td><input type="password" name="add_pass" /></td></tr>
 						<tr><td>Real Name:</td><td><input type="text" name="add_real" autocomplete="off" /></td></tr>
 						<tr><td>Role:</td><td>
-						<?php
-                        $permissions = new Permissions();
-                        $list = $permissions->getGroupList();
-                        
-                        echo '<select name="add_role">';
+						<select name="add_role">
+HTML;
                         foreach ($list as $group) {
                             echo '<option value="'.$group['role'].'">'.ucfirst($group['role']).'</option>';
                         }
-                        echo '</select>';
-						?>
+		echo <<<HTML
+						</select>
 						</td></tr>
 					</table>
 					<input type="submit" name="sub_type" value="Add">
 					<input type="submit" name="sub_type" value="Cancel">
 				</form>
 		</div><br>
-	<?php
+HTML;
 	}
 
 	/** Confirm user delete form
@@ -168,13 +168,13 @@ class UserForms
 	 * @return nothing
 	 */
 	public function resetPassword($uid, $uname, $realname) {
-	?>
+		echo <<<HTML
 		<div id="editform">
-			<h2>Reset Password for <?php echo $realname; ?>:</h2>
+			<h2>Reset Password for {$realname}:</h2>
 			<form name="edit_form" method="post">
 				<table>
-					<tr><td>User ID:</td><td><input type="text" name="reset_uid" value="<?php echo $uid; ?>" readonly /></td></tr>
-					<tr><td>Username:</td><td><input type="text" value="<?php echo $uname; ?>" readonly /></td></tr>
+					<tr><td>User ID:</td><td><input type="text" name="reset_uid" value="{$uid}" readonly /></td></tr>
+					<tr><td>Username:</td><td><input type="text" value="{$uname}" readonly /></td></tr>
 					<tr><td>New Password:</td><td><input type="password" name="reset_1" /></td></tr>
 					<tr><td>Repeat Password:</td><td><input type="password" name="reset_2" /></td></tr>
 				</table>
@@ -182,6 +182,6 @@ class UserForms
 				<input type="submit" name="sub_type" value="Cancel" />
 			</form>
 		</div><br />
-	<?php
+HTML;
 	}
 }
