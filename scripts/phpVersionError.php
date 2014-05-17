bfc5d66243e1a69dcdfa97ca65b0a9cacf98d7f7
dragonrider23 <keitellf@outlook.com>
2014-05-17 10:15:20 -0500

    Attribution

--- scripts/phpVersionError.php
+++ scripts/phpVersionError.php
@@ -16,6 +16,8 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
+ *
+ * This method was taken from the MediaWiki project.s
  */
 
 /**
