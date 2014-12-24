7d634104968ca6f40c5bb5ad5ccb427d588b7857
Lee Keitel <keitellf@gmail.com>
2014-12-24 13:45:13 -0600

    Updated comment to reflect new usage

--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <?php
 /**
-  * This is the homepage of Dandelion.
-  * If a user is already logged in it will
-  * redirect them to the viewlog page.
+  * This is the main entry point of Dandelion. All page and API requests are routed here.
   *
   * This file is a part of Dandelion
   *
   * @author Lee Keitel
-  * @date March 2014
+  * @date December 2014
   *
   * @license GNU GPL v3 (see full license in root/LICENSE.md)
 ***/
 namespace Dandelion;
 
-/*! \mainpage Dandelion Weblog
- *
- *  \section intro_sec Introduction
- *
- * Dandelion was conceived after thinking of a way to replace the aging and slow Bloxom-based
- * log we were currently using to document change logs. I wanted to keep the web-based system,
- * use an SQL database instead of text files, and make every action possible via the browser
- * instead of having to SSH into the Blosxom server.
- *
- * And that is how Dandelion was born.
- */
 require_once 'lib/bootstrap.php';
 
 // Load installer if necassary
