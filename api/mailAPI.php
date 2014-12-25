7310561441a18af06318b1e57d3f33109a175d08
Lee Keitel <keitellf@gmail.com>
2014-12-25 08:42:54 -0600

    Converted mail api to new database object

--- api/mailAPI.php
+++ api/mailAPI.php
@@ -29,63 +29,63 @@ class mailAPI
 {
     /**
      * Grab JSON array of all cheesto users and statuses
-     * 
+     *
      * @return JSON
      */
-    public static function read() {
-        $myMail = new \Dandelion\Mail\mail();
+    public static function read($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
 
         $mail = $myMail->getFullMailInfo($_REQUEST['mid']);
         $myMail->setReadMail($_REQUEST['mid']);
-        
+
         return json_encode($mail);
     }
-    
-    public static function mailCount() {
-        $myMail = new \Dandelion\Mail\mail();
-        
+
+    public static function mailCount($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
+
         $count = $myMail->checkNewMail(true);
         $count = array( 'count' => $count);
-        
+
         return json_encode($count);
     }
-    
-    public static function delete() {
-        $myMail = new \Dandelion\Mail\mail();
-        
+
+    public static function delete($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
+
         $perm = ($_REQUEST['permenant'] === 'true') ? true : false;
         $response = $myMail->deleteMail($_REQUEST['mid'], $perm);
-        
+
         return json_encode($response);
     }
-    
-    public static function getUserList() {
-        $myMail = new \Dandelion\Mail\mail();
-        
+
+    public static function getUserList($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
+
         $toUsers = $myMail->getUserList();
-        
+
         return json_encode($toUsers);
     }
-    
-    public static function getAllMail() {
-        $myMail = new \Dandelion\Mail\mail();
-        
+
+    public static function getAllMail($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
+
         if (isset($_REQUEST['trash']) && $_REQUEST['trash']) {
             $mailItems = $myMail->getTrashCan();
         }
         else {
             $mailItems = $myMail->getMailList();
         }
-        
+
         return json_encode($mailItems);
     }
-    
-    public static function send() {
-        $myMail = new \Dandelion\Mail\mail();
-        
+
+    public static function send($db) {
+        $myMail = new \Dandelion\Mail\mail($db);
+
         $piece = (array) json_decode($_REQUEST['mail']);
         $response = $myMail->newMail($piece['subject'], $piece['body'], $piece['to'], $_SESSION['userInfo']['userid']);
-        
+
         return json_encode($response);
     }
 }
--- classes/mail.php
+++ classes/mail.php
@@ -19,159 +19,140 @@
  *
  * @author Lee Keitel
  *         @date June 2014
- *         *
+ *
  */
 namespace Dandelion\Mail;
 
-use Dandelion\Database;
-
-class mail extends Database\dbManage
+class mail
 {
+    public function __construct($db) {
+        $this->dbConn = $db;
+        return;
+    }
 
-    function checkNewMail($forced = false, $sent = false)
+    public function checkNewMail($forced = false, $sent = false)
     {
         $mailCount = -255;
-        
+
         if ($forced) {
             // Get number of unread mail items
             $mailItems = $this->getMailList($sent, true);
             $mailCount = count($mailItems);
-            
+
             // Update user's mail count
-            $sql = 'UPDATE ' . DB_PREFIX . 'users
-                    SET mailCount = :mc
-                    WHERE userid = :id';
+            $this->dbConn->update(DB_PREFIX.'users')
+                          ->set('mailcount = :mc')
+                          ->where('userid = :id');
             $params = array (
                 'mc' => $mailCount,
-                'id' => $_SESSION['userInfo']['userid'] 
+                'id' => $_SESSION['userInfo']['userid']
             );
-            
-            $this->queryDB($sql, $params);
+            $this->dbConn->go($params);
         }
         else {
-            $sql = 'SELECT mailCount
-                    FROM ' . DB_PREFIX . 'users
-                    WHERE userid = :id';
+            $this->dbConn->select('mailCount')
+                          ->from(DB_PREFIX.'users')
+                          ->where('userid = :id');
             $params = array (
-                'id' => $_SESSION['userInfo']['userid'] 
+                'id' => $_SESSION['userInfo']['userid']
             );
-            
-            $mailCount = $this->queryDB($sql, $params)[0]['mailCount'];
+            $mailCount = $this->dbConn->get($params)[0]['mailCount'];
         }
-        
-        return $mailCount;
+
+        return (int) $mailCount;
     }
 
-    function getMailList($sent = false, $unread = false)
+    public function getMailList($sent = false, $unread = false)
     {
         $toFrom = ($sent) ? 'fromUser' : 'toUser';
         $unreadCond = ($unread) ? ' AND isItRead = 0' : '';
-        
-        $sql = 'SELECT m.id, m.isItRead, m.subject, m.fromUser, m.dateSent,
-                    u.realname
-                FROM ' . DB_PREFIX . 'mail AS m
-                LEFT JOIN ' . DB_PREFIX . 'users AS u
-                ON m.fromUser = u.userid
-                WHERE m.' . $toFrom . ' = :id' . $unreadCond . ' AND deleted = 0';
-        
+
+        $this->dbConn->select('m.id, m.isItRead, m.subject, m.fromUser, m.dateSent, u.realname')
+                      ->from(DB_PREFIX.'mail AS m LEFT JOIN '.DB_PREFIX.'users AS u ON m.fromUser = u.userid')
+                      ->where('m.'.$toFrom.' = :id'.$unreadCond.' AND deleted = 0');
         $params = array (
-            'id' => $_SESSION['userInfo']['userid'] 
+            'id' => $_SESSION['userInfo']['userid']
         );
-        
-        return $this->queryDB($sql, $params);
+        return $this->dbConn->get($params);
     }
 
-    function getFullMailInfo($mailId)
+    public function getFullMailInfo($mailId)
     {
-        $sql = 'SELECT m.*, u.realname
-                FROM ' . DB_PREFIX . 'mail AS m
-                LEFT JOIN ' . DB_PREFIX . 'users AS u
-                ON m.fromUser = u.userid
-                WHERE id = :mid';
-        
+        $this->dbConn->select('m.*, u.realname')
+                      ->from(DB_PREFIX.'mail AS m LEFT JOIN '.DB_PREFIX.'users AS u ON m.fromUser = u.userid')
+                      ->where('id = :mid');
         $params = array (
-            'mid' => $mailId 
+            'mid' => $mailId
         );
-        
-        return $this->queryDB($sql, $params);
+        return $this->dbConn->get($params);
     }
 
-    function getUserList()
+    public function getUserList()
     {
-        $sql = 'SELECT userid, realname
-                FROM ' . DB_PREFIX . 'users
-                ORDER BY realname';
-        
-        return $this->queryDB($sql);
+        $this->dbConn->select('userid, realname')
+                      ->from(DB_PREFIX.'users')
+                      ->orderBy('realname');
+        return $this->dbConn->get();
     }
 
-    function getTrashCan()
+    public function getTrashCan()
     {
-        $sql = 'SELECT m.*, u.realname
-                FROM ' . DB_PREFIX . 'mail AS m
-                LEFT JOIN '. DB_PREFIX.'users AS u
-                ON m.fromUser = u.userid
-                WHERE m.toUser = :id AND m.deleted = 1';
-        
+        $this->dbConn->select('m.*, u.realname')
+                      ->from(DB_PREFIX.'mail AS m LEFT JOIN '.DB_PREFIX.'users AS u ON m.fromUser = u.userid')
+                      ->where('m.toUser = :id AND m.deleted = 1');
         $params = array (
-            'id' => $_SESSION['userInfo']['userid'] 
+            'id' => $_SESSION['userInfo']['userid']
         );
-        
-        return $this->queryDB($sql, $params);
+
+        return $this->dbConn->get($params);
     }
 
-    function newMail($subject, $body, $to, $from)
+    public function newMail($subject, $body, $to, $from)
     {
         if (!empty($subject) && !empty($body) && !empty($to) && !empty($from)) {
             $datetime = getdate();
             $new_date = $datetime['year'] . '-' . $datetime['mon'] . '-' . $datetime['mday'];
             $new_time = $datetime['hours'] . ':' . $datetime['minutes'] . ':' . $datetime['seconds'];
-            
-            $sql = 'INSERT INTO ' . DB_PREFIX . 'mail
-                    (subject, body, toUser, fromUser, dateSent, timeSent)
-                    VALUES (:sub, :body, :to, :from, :date, :time)';
-            
+
+            $this->dbConn->insert()->into(DB_PREFIX.'mail', array('subject','body','toUser','fromUser','dateSent','timeSent'))
+                          ->values(array(':sub', ':body', ':to', ':from', ':date', ':time'));
             $params = array (
                 'sub' => $subject,
                 'body' => $body,
                 'to' => $to,
                 'from' => $from,
                 'date' => $new_date,
-                'time' => $new_time 
+                'time' => $new_time
             );
-            
-            $this->queryDB($sql, $params);
-            
-            $sql = 'UPDATE ' . DB_PREFIX . 'users
-                    SET mailCount = mailCount + 1
-                    WHERE userid = :id';
-            
+            $this->dbConn->go($params);
+
+            $this->dbConn->update(DB_PREFIX.'users')
+                          ->set('mailCount = mailCount + 1')
+                          ->where('userid = :id');
             $params = array( 'id' => $to );
-            
-            $this->queryDB($sql, $params);
-            
+            $this->dbConn->go($params);
+
             $response = 'Mail sent successfully';
         }
         else {
             $response = 'Error: You need a subject, body, and recipient.';
         }
-        
+
         return $response;
     }
 
-    function setReadMail($mid)
+    public function setReadMail($mid)
     {
-        $sql = 'UPDATE ' . DB_PREFIX . 'mail
-                SET isItRead = 1
-                WHERE id = :id';
+        $this->dbConn->update(DB_PREFIX.'mail')
+                      ->set('isItRead = 1')
+                      ->where('id = :id');
         $params = array (
-            'id' => $mid 
+            'id' => $mid
         );
-        
-        return $this->queryDB($sql, $params);
+        return $this->dbConn->go($params);
     }
 
-    function deleteMail($mailId, $trueDelete = false)
+    public function deleteMail($mailId, $trueDelete = false)
     {
         if ($mailId === -1) {
             // Permanently delete ALL mail
@@ -179,24 +160,22 @@ class mail extends Database\dbManage
             // aka, empty trash
         }
         else {
-            $sql = '';
-
             if ($trueDelete) {
                 // Complete delete
-                $sql = 'DELETE
-                        FROM '. DB_PREFIX.'mail
-                        WHERE id = :mid';
+                $this->dbConn->delete()
+                              ->from(DB_PREFIX.'mail')
+                              ->where('id = :mid');
             }
             else {
                 // Soft delete
-                $sql = 'UPDATE ' . DB_PREFIX . 'mail
-                        SET deleted = 1
-                        WHERE id = :mid';
+                $this->dbConn->update(DB_PREFIX.'mail')
+                              ->set('deleted = 1')
+                              ->where('id = :mid');
             }
-                
+
             $params = array( 'mid' => $mailId );
-            
-            if ($this->queryDB($sql, $params)) {
+
+            if ($this->dbConn->go($params)) {
                 return 'Mail Deleted';
             }
             else {
