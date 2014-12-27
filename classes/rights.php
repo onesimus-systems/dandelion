5fc0467a3b2e192d1322fccd6c21ce26b6fcff4a
Lee Keitel <keitellf@gmail.com>
2014-12-27 16:13:48 -0600

    Save user ID in rights object

--- classes/rights.php
+++ classes/rights.php
@@ -17,30 +17,31 @@ use Dandelion\Permissions;
 class rights
 {
     private $permissions = null;
-    
+
     /**
      * Load permissions of specified user
-     * 
+     *
      * @param int $userid - ID of user
      */
     public function __construct($userid) {
-        $sql = 'SELECT r.permissions
-                FROM '.DB_PREFIX.'rights AS r
-                LEFT JOIN '.DB_PREFIX.'users AS u
-                    ON u.role = r.role
-                WHERE u.userid = :userid';
-        $params = array(
-        	'userid' => $userid
-        );
-        
-        $conn = new dbManage();
-        $rights = $conn->queryDB($sql, $params);
-        $this->permissions = (array) unserialize($rights[0]['permissions']);
+      $this->userid = $userid;
+      $sql = 'SELECT r.permissions
+              FROM '.DB_PREFIX.'rights AS r
+              LEFT JOIN '.DB_PREFIX.'users AS u
+                  ON u.role = r.role
+              WHERE u.userid = :userid';
+      $params = array(
+      	'userid' => $userid
+      );
+
+      $conn = new dbManage();
+      $rights = $conn->queryDB($sql, $params);
+      $this->permissions = (array) unserialize($rights[0]['permissions']);
     }
-    
+
     /**
      * Check if user is authorized for requested action
-     * 
+     *
      * @param string $permission - Permission to check
      */
     public function authorized($permission) {
@@ -48,10 +49,10 @@ class rights
         if ($this->permissions['admin']) {
             return true;
         }
-        
+
         return $this->permissions[strtolower($permission)];
     }
-    
+
     public function isAdmin() {
         return $this->permissions['admin'];
     }
