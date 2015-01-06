463188e041c39b5223583caa9cb8b50107f11e2c
Lee Keitel <keitellf@gmail.com>
2015-01-05 21:39:46 -0600

    Created API for user management

---
+++ api/usersAPI.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Handles API requests for user management
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * The full GPLv3 license is available in LICENSE.md in the root.
+ *
+ * @author Lee Keitel
+ * @date January 2015
+ */
+namespace Dandelion\API;
+
+if (REQ_SOURCE != 'api' && REQ_SOURCE != 'iapi') {
+    exit(makeDAPI(2, 'This script can only be called by the API.', 'users'));
+}
+
+class usersAPI
+{
+    public static function resetPassword($db, $ur) {
+        $userid = USER_ID;
+
+        // Check permissions
+        if (isset($_REQUEST['uid'])) {
+            if ($ur->authorized('edituser') || $_REQUEST['uid'] == USER_ID) {
+                $userid = $_REQUEST['uid'];
+            } else {
+                exit(makeDAPI(4, 'This account doesn\'t have the proper permissions.', 'users'));
+            }
+        }
+
+        // Validate password
+        $newPass = $_REQUEST['pw'];
+        if ($newPass == '' || $newPass == null) {
+            exit(makeDAPI(5, 'New password cannot be empty', 'users'));
+            return;
+        }
+
+        // Do action
+        $user = new \Dandelion\Users($db, $userid);
+        return json_encode($user->resetPassword($newPass));
+    }
+
+    public static function create($db, $ur) {
+        if (!$ur->authorized('adduser')) {
+            exit(makeDAPI(4, 'This account doesn\'t have the proper permissions.', 'users'));
+        }
+
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $realname = $_REQUEST['fullname'];
+        $role     = $_REQUEST['role'];
+        //$cheesto = $_REQUEST['makecheesto'];
+
+        $user = new \Dandelion\Users($db);
+        return json_encode($user->createUser($username, $password, $realname, $role));
+    }
+
+    public static function save($db, $ur) {
+        if (!$ur->authorized('edituser')) {
+            exit(makeDAPI(4, 'This account doesn\'t have the proper permissions.', 'users'));
+        }
+
+        $uid = $_REQUEST['uid'];
+        if (empty($uid)) {
+            exit(makeDAPI(5, 'No user id received.', 'users'));
+        }
+
+        $user = new \Dandelion\Users($db, $uid, true);
+        $user->userInfo['realname']  = isset($_REQUEST['fullname']) ? $_REQUEST['fullname'] : $user->userInfo['realname'];
+        $user->userInfo['role']  = isset($_REQUEST['role']) ? $_REQUEST['role'] : $user->userInfo['role'];
+        $user->userInfo['firsttime']  = isset($_REQUEST['prompt']) ? $_REQUEST['prompt'] : $user->userInfo['firsttime'];
+        $user->userInfo['theme']  = isset($_REQUEST['theme']) ? $_REQUEST['theme'] : $user->userInfo['theme'];
+        return json_encode($user->saveUser());
+    }
+
+    public static function delete($db, $ur) {
+        if (USER_ID == $_REQUEST['uid']) {
+            exit(makeDAPI(5, 'You can\'t delete yourself.', 'users'));
+        }
+
+        // Check permissions
+        if ($ur->authorized('edituser')) {
+            $userid = $_REQUEST['uid'];
+        } else {
+            exit(makeDAPI(4, 'This account doesn\'t have the proper permissions.', 'users'));
+        }
+
+        $user = new \Dandelion\Users($db);
+        return json_encode($user->deleteUser($userid));
+    }
+}
---
+++ classes/users.php
@@ -0,0 +1,284 @@
+<?php
+
+/**
+ * Handle user management functions
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * The full GPLv3 license is available in LICENSE.md in the root.
+ *
+ * @author Lee Keitel
+ * @date Feb 2014
+ */
+namespace Dandelion;
+
+use Dandelion\Permissions;
+
+class Users
+{
+    // User data
+    public $userInfo = array();
+    public $userCheesto = array();
+    public $userApi = array();
+
+    public function __construct($db, $uid = -1, $load = false) {
+        $this->db = $db;
+
+        if ($uid >= 0) {
+            $this->userInfo['userid'] = $uid;
+        } else {
+            $this->userInfo['userid'] = null;
+        }
+
+        if ($load === true && $uid >= 0) {
+            $this->loadUser();
+        } elseif ($load === true && $uid < 0) {
+            trigger_error('To load a user you must provide a user ID.');
+        }
+        return true;
+    }
+
+    public function loadUser() {
+        $this->db->select('u.userid AS u_userid,
+                            u.username AS u_username,
+                            u.realname AS u_realname,
+                            u.role AS u_role,
+                            u.firsttime AS u_firsttime,
+                            u.theme AS u_theme,
+                            u.datecreated AS u_datecreated,
+                        p.id AS p_id,
+                            p.status AS p_status,
+                            p.message AS p_message,
+                            p.returntime AS p_returntime,
+                        a.keystring AS a_keystring,
+                            a.expires AS a_expires,
+                        r.permissions AS u_permissions')
+                 ->from(DB_PREFIX . 'users AS u
+                    LEFT JOIN ' . DB_PREFIX . 'presence AS p
+                        ON p.uid = u.userid
+                    LEFT JOIN ' . DB_PREFIX . 'apikeys AS a
+                        ON a.user = u.userid
+                    LEFT JOIN ' . DB_PREFIX . 'rights AS r
+                        ON r.role = u.role')
+                 ->where('u.userid = :userid');
+
+        $params = array(
+            'userid' => $this->userInfo['userid']
+        );
+
+        $allUserInfo = $this->db->get($params);
+
+        foreach ($allUserInfo[0] as $key => $value) {
+            $infoType = substr($key, 0, 2);
+            $key = substr_replace($key, '', 0, 2);
+
+            switch ($infoType) {
+                case 'u_':
+                    if ($key == 'permissions') {
+                        $value = (array) unserialize($value);
+                    }
+                    $this->userInfo[$key] = $value;
+                    break;
+                case 'p_':
+                    $this->userCheesto[$key] = $value;
+                    break;
+                case 'a_':
+                    $this->userApi[$key] = $value;
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    public function saveUser() {
+        if (empty($this->userInfo['realname'])
+            || empty($this->userInfo['role'])
+            || (empty($this->userInfo['firsttime']) && $this->userInfo['firsttime'] != 0)
+            || empty($this->userInfo['userid'])) {
+            return 'Something is empty';
+        }
+
+        // Update main user row
+        $this->db->update(DB_PREFIX.'users')
+                 ->set('realname = :realname, role = :role, firsttime = :first, theme = :theme')
+                 ->where('userid = :userid');
+        $params = array(
+            'realname' => $this->userInfo['realname'],
+            'role' => $this->userInfo['role'],
+            'first' => $this->userInfo['firsttime'],
+            'userid' => $this->userInfo['userid'],
+            'theme' => $this->userInfo['theme']
+        );
+        $this->db->go($params);
+
+        // Update Cheesto information
+        $this->db->update(DB_PREFIX.'presence')
+                 ->set('realname = :realname')
+                 ->where('uid = :userid');
+        $params = array(
+            'realname' => $this->userInfo['realname'],
+            'userid' => $this->userInfo['userid']
+        );
+
+        if (!$this->db->go($params)) {
+            return 'There was an error saving the user';
+        }
+
+        return true;
+    }
+
+    public function createUser($username, $password, $realname, $role, $cheesto = true) {
+        $date = new \DateTime();
+
+        // Error checking
+        if (empty($username) || empty($password) || empty($realname) || empty($role)) {
+            return 'Something is empty';
+        }
+        if ($this->isUser($username)) {
+            return 'Username already in use';
+        }
+
+        // Create row in users table
+        $this->db->insert()
+                 ->into(DB_PREFIX.'users', array('username', 'password', 'realname', 'role', 'datecreated', 'theme'))
+                 ->values(array(':username', ':password', ':realname', ':role', ':datecreated', '\'\''));
+        $params = array(
+            'username' => $username,
+            'password' => password_hash($password, PASSWORD_BCRYPT),
+            'realname' => $realname,
+            'role' => $role,
+            'datecreated' => $date->format('Y-m-d')
+        );
+        $this->db->go($params);
+
+        if ($cheesto) {
+            $lastID = $this->db->lastInsertId();
+
+            // Create row in presence table
+            $this->db->insert()
+                     ->into(DB_PREFIX.'presence', array('uid', 'realname', 'status', 'message', 'returntime', 'dmodified'))
+                     ->values(array(':uid', ':real', 0, '\'\'', '\'00:00:00\'', ':date'));
+            $params = array(
+                'uid' => $lastID,
+                'real' => $realname,
+                'date' => $date->format('Y-m-d H:i:s')
+            );
+
+            $this->db->go($params);
+        }
+
+        return true;
+    }
+
+    private function isUser($username) {
+        $this->db->select()
+                 ->from(DB_PREFIX.'users')
+                 ->where('username = :username');
+        $params = array(
+            'username' => $username
+        );
+        $row = $this->db->get($params);
+        return !empty($row);
+    }
+
+    public function resetPassword($pass = '') {
+        $uid = $this->userInfo['userid'];
+
+        if (empty($uid) || empty($pass)) {
+            return 'Something is empty';
+        }
+
+        $pass = password_hash($pass, PASSWORD_BCRYPT);
+
+        $this->db->update(DB_PREFIX.'users')->set('password = :newpass, firsttime = 0')->where('userid = :id');
+        $params = array(
+            'newpass' => $pass,
+            'id' => $uid
+        );
+
+        if ($this->db->go($params)) {
+            return true;
+        }
+        else {
+            return 'Error changing password.';
+        }
+    }
+
+    public function deleteUser($uid) {
+        if (empty($uid)) {
+            if (!empty($this->userInfo['userid'])) {
+                $uid = $this->userInfo['userid'];
+            } else {
+                return 'No user id provided';
+            }
+        }
+
+        $delete = false;
+
+        $this->db->select('role')
+                 ->from(DB_PREFIX.'users')
+                 ->where('userid = :userid');
+        $params = array(
+            'userid' => $uid
+        );
+        $userRole = $this->db->get($params)[0]['role'];
+
+        $perms = new Permissions($this->db);
+        $isAdmin = (array) $perms->loadRights($userRole);
+
+        if (!$isAdmin['admin']) {
+            // If the account being deleted isn't an admin, then there's nothing to worry about
+            $delete = true;
+        }
+        else {
+            // If the account IS an admin, check all other users to make sure
+            // there's at least one other user with the admin rights flag
+            $this->db->select('role')
+                     ->from(DB_PREFIX.'users')
+                     ->where('userid != :userid');
+            $params = array(
+                'userid' => $uid
+            );
+            $otherUsers = $this->db->get($params);
+
+            foreach ($otherUsers as $areTheyAdmin) {
+                $isAdmin = (array) $perms->loadRights($areTheyAdmin['role']);
+
+                if ($isAdmin['admin']) {
+                    // If one is found, stop for loop and allow the delete
+                    $delete = true;
+                    break;
+                }
+            }
+        }
+
+        if ($delete) {
+            $this->db->delete('u, p, a, m')
+                     ->from(DB_PREFIX . 'users AS u LEFT JOIN ' . DB_PREFIX . 'presence AS p ON p.uid = u.userid LEFT JOIN ' . DB_PREFIX . 'apikeys AS a ON a.user = u.userid LEFT JOIN ' . DB_PREFIX . 'mail AS m ON m.toUser = u.userid')
+                     ->where('u.userid = :userid');
+            $params = array(
+                'userid' => $uid
+            );
+
+            if ($this->db->go($params)) {
+                return true;
+            } else {
+                return 'Error deleting user';
+            }
+        }
+        else {
+            return 'At least one admin account must be left to delete another admin account';
+        }
+    }
+}
