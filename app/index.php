50914c0ceb34f83d93883f805df6f1f6af59d860
Lee Keitel <lfkeitel@usi.edu>
2015-01-23 12:59:07 -0600

    Objectify the entire application
    The only remaining access point is the categories library

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
   * This file is a part of Dandelion
   *
   * @author Lee Keitel
-  * @date December 2014
+  * @date Janurary 23
   *
   * @license GNU GPL v3 (see full license in root/LICENSE.md)
 ***/
@@ -13,39 +13,99 @@ namespace Dandelion;
 
 require_once 'lib/bootstrap.php';
 
-// Load installer if necassary
-if (!INSTALLED) {
-    redirect('installer');
+/**
+ * DandelionApplication represents an instance of Dandelion. It requires
+ * the URI as a parameter and has one public method run(). The class parses
+ * the URI, stores it and when run() is executed will route the request accordingly.
+ */
+class DandelionApplication {
+    private $url = [];
+
+    /**
+     *  @param $url string - The request URI
+     */
+    public function __construct($url) {
+        // Load installer if necassary
+        if (!INSTALLED) {
+            redirect('installer');
+        }
+
+        $this->url = $this->parseUrl($url);
+    }
+
+    /**
+     * Parses given URI string and returns an array of the pieces
+     *
+     * @param $url string - Request URI
+     * @return array - Exploded URL
+     */
+    private function parseUrl($url) {
+        // Process url
+        $url = $url ? $url : '';
+        $url = explode('/', $url);
+        array_shift($url); // Remove empty index
+        return $url;
+    }
+
+    /**
+     * Main function of this class and single entrypoint into application.
+     * Run takes the parsed URL and routes it to the appropiate place be it
+     * the api controller or a page.
+     */
+    public function run() {
+        // Get the first element which is the page or command
+        $page = array_shift($this->url);
+
+        // Call the API if it's an api call
+        if ($page == 'api') {
+            $this->apiRequest();
+            return;
+        }
+
+        $this->loadPage($page);
+        return;
+    }
+
+    /**
+     * Processes and api request
+     */
+    private function apiRequest() {
+        header('Content-Type: application/json');
+        $apiCall = new API\ApiController();
+        $apiCall->processCall($this->url);
+        return;
+    }
+
+    /**
+     * Loads the given page
+     *
+     * @param $page string - Name of page to load
+     */
+    private function loadPage($page) {
+        global $User_Rights;
+        // Set the homepage if necassary
+        if ($page === '') {
+          $page = "viewlog";
+        }
+
+        // Load page
+        $indexCall = true;
+        if (is_file('pages/'.$page.'.php') && Gatekeeper\authenticated()) {
+            include 'pages/'.$page.'.php';
+        } else {
+            include 'pages/login.php';
+        }
+        return;
+    }
+
+    /**
+     * Gotta make sure the session is written and closed
+     */
+    public function __destruct() {
+        session_write_close();
+    }
 }
 
-// Process url
-$url = isset($_REQUEST['url']) ? $_REQUEST['url'] : '';
-$url = explode('/', $url);
-
-// Get the first element which is the main page or command
-$mainCall = array_shift($url);
-
-// Call the API if it's an api call
-if ($mainCall == 'api') {
-  header('Content-Type: application/json');
-  $apiCall = new API\ApiController();
-  $apiCall->processCall($url);
-  session_write_close();
-  exit(0);
-}
-
-// Set the homepage if necassary
-if ($mainCall == "") {
-  // Homepage
-  $mainCall = "viewlog";
-}
-
-// Load page
-$indexCall = true;
-if (is_file('pages/'.$mainCall.'.php') && Gatekeeper\authenticated()) {
-    include 'pages/'.$mainCall.'.php';
-} else {
-    include 'pages/login.php';
-}
-
-session_write_close();
+// Instantiate and run application
+$app = new DandelionApplication($_SERVER['REQUEST_URI']);
+$app->run();
