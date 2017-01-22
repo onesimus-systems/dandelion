c39f1f02ad1bffcc4dfa04ac5f22bd7642643cbb
Lee Keitel <lee@keitel.xyz>
2017-01-22 15:57:18 -0600

    Bumped patch version to 6.1.1.

--- app/Dandelion/Application.php
+++ app/Dandelion/Application.php
@@ -33,7 +33,7 @@ class Application
     /**
      * @const string Current version number
      */
-    const VERSION = '6.1.0';
+    const VERSION = '6.1.1';
 
     /**
      * @const string Current version name
