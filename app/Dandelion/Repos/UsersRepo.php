bc0824cfedf23d85db909a5213f14518f2af07b0
Lee Keitel <lee@keitel.xyz>
2017-01-22 22:01:26 -0600

    Cleaned up unused interface and repo methods
    I don't know why I haven't removed these completely but I haven't

--- app/Dandelion/Repos/UsersRepo.php
+++ app/Dandelion/Repos/UsersRepo.php
@@ -21,63 +21,9 @@ class UsersRepo extends BaseRepo implements Interfaces\UsersRepo
         $this->table = $this->prefix.'user';
     }
 
-    public function saveUser($uid, $realname, $role, $theme, $first, $disabled)
-    {
-        return $this->database->updateItem($this->table, $uid, [
-            'fullname' => $realname,
-            'group_id' => $role,
-            'initial_login' => $first,
-            'theme' => $theme,
-            'disabled' => $disabled
-        ]);
-    }
-
-    public function createUser($username, $password, $fullname, $role, $date)
-    {
-        return $this->database->createItem($this->table, [
-            'username' => $username,
-            'password' => $password,
-            'fullname' => $fullname,
-            'group_id' => $role,
-            'created'  => $date
-        ]);
-    }
-
-    public function createUserCheesto($uid, $date)
-    {
-        return $this->database->createItem($this->prefix.'cheesto', [
-            'status' => 'Available',
-            'message' => '',
-            'returntime' => '00:00:00',
-            'modified' => $date,
-            'user_id' => $uid
-        ]);
-    }
-
-    public function isUser($username)
-    {
-        return $this->database
-            ->find($this->table)
-            ->whereEqual('username', $username)
-            ->whereEqual('disabled', 0)
-            ->readRecord();
-    }
-
-    public function resetPassword($uid, $pass)
-    {
-        return $this->database
-            ->updateItem($this->table, $uid, ['password' => $pass, 'initial_login' => 0]);
-    }
-
     public function deleteUser($uid)
     {
         return $this->database->deleteItem($this->table, $uid);
-    }
-
-    public function disableUser($uid, $disabled = 1)
-    {
-        return $this->database
-            ->updateItem($this->table, $uid, ['disabled' => $disabled]);
     }
 
     public function getUserRole($uid, $invert = false)
